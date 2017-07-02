9e0602f840f7d1e54ddc5c4397cf45cd5c32ff23
I <vince@oomphhq.com>
2017-07-03 09:20:34 +1000

    initial commit webpack skeleton

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # neo-wallet-js
-Javascript libraries for neo wallet
+
+Javascript libraries for neo wallet using https://github.com/neochainio/neowallet/blob/master/js/wallet.js as the original source.
+
+### To build to /dist:
+```
+npm build
+```
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+      path: __dirname,
+      filename: './dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')]
+          }
+        }
+      }
+    ]
+  }
+}
