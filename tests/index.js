9da7df1068f3d052ba62a539d17265b5fbef2099
I <vince@oomphhq.com>
2017-07-06 17:47:43 +1000

    correct public keys

--- tests/index.js
+++ tests/index.js
@@ -29,14 +29,14 @@ describe('Wallet', () => {
     address1: {
       address: "ALfnhLg7rUyL6Jr98bzzoxz5J7m64fbR4s",
       privKey: "9ab7e154840daca3a2efadaf0df93cd3a5b51768c632f5433f86909d9b994a69",
-      pubKeyEncoded:"021d8e1630ce640966967bc6d95223d21f44304133003140c3b52004dc981349c9",
+      pubKeyEncoded:"031d8e1630ce640966967bc6d95223d21f44304133003140c3b52004dc981349c9",
       wif: "L2QTooFoDFyRFTxmtiVHt5CfsXfVnexdbENGDkkrrgTTryiLsPMG"
     },
 
     address2: {
       address: "AVf4UGKevVrMR1j3UkPsuoYKSC4ocoAkKx",
       privKey: "3edee7036b8fd9cef91de47386b191dd76db2888a553e7736bb02808932a915b",
-      pubKeyEncoded: "02232ce8d2e2063dce0451131851d47421bfc4fc1da4db116fca5302c0756462fa",
+      pubKeyEncoded: "03232ce8d2e2063dce0451131851d47421bfc4fc1da4db116fca5302c0756462fa",
       wif: "KyKvWLZsNwBJx5j9nurHYRwhYfdQUu9tTEDsLCUHDbYBL8cHxMiG"
     }
   }
