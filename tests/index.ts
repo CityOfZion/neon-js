f26c37c25d5929438a8c140e2074cc46ea28c0ec
ricky han <rickylqhan@gmail.com>
2017-08-11 01:07:56 -0400

    test typechecks

--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,4 @@
-import chai from 'chai';
+import 'chai';
 import { ab2str,
   str2ab,
   hexstring2ab,
@@ -6,7 +6,7 @@ import { ab2str,
   reverseArray,
   numStoreInMemory,
   stringToBytes } from '../src/utils';
-import * as api from '../lib/index';
+import * as api from '../src/api';
 import axios from 'axios';
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
@@ -50,13 +50,19 @@ describe('Wallet', function() {
   it('should generate a valid WIF', (done) => {
     const privateKey = api.generatePrivateKey();
     const wif = api.getWIFFromPrivateKey(privateKey);
-    const account = api.getAccountsFromWIFKey(wif)[0];
+    const accounts = api.getAccountsFromWIFKey(wif);
+    if (accounts === -1 || accounts === -2)
+      throw "Accounts error";
+    const account = accounts[0];
     account.privatekey.should.equal(ab2hexstring(privateKey));
     done();
   });
 
   it('should get keys from a WIF', (done) =>{
-    const account = api.getAccountsFromWIFKey(testKeys.a.wif)[0];
+    const accounts = api.getAccountsFromWIFKey(testKeys.a.wif);
+    if (accounts === -1 || accounts === -2)
+      throw "Accounts error";
+    const account = accounts[0];
     account.privatekey.should.be.a('string');
     account.address.should.equal(testKeys.a.address);
     done();
@@ -66,6 +72,7 @@ describe('Wallet', function() {
     const privateKey = ab2hexstring(api.generatePrivateKey());
     const accounts = api.getAccountsFromPrivateKey(privateKey);
     accounts.should.not.equal(-1);
+    if (accounts === -1) return;
     const verify = api.verifyPublicKeyEncoded(accounts[0].publickeyEncoded);
     verify.should.equal(true);
     done();
@@ -75,13 +82,14 @@ describe('Wallet', function() {
     const privateKey = ab2hexstring(api.generatePrivateKey());
     const accounts = api.getAccountsFromPrivateKey(privateKey);
     accounts.should.not.equal(-1);
+    if (accounts === -1) return;
     const verify = api.verifyAddress(accounts[0].address);
     verify.should.equal(true);
     done();
   });
 
   it('should get balance from address', (done) => {
-    api.getBalance(api.TESTNET, testKeys.a.address).then((response) =>{
+    api.getBalance(api.getAPIEndpoint('testnet'), testKeys.a.address).then((response) =>{
       response.Neo.should.be.a('number');
       response.Gas.should.be.a('number');
       done();
@@ -89,7 +97,7 @@ describe('Wallet', function() {
   });
 
   it('should get unspent transactions', (done) => {
-    api.getBalance(api.TESTNET, testKeys.a.address, api.ansId).then((response) => {
+    api.getBalance(api.getAPIEndpoint('testnet'), testKeys.a.address).then((response) => {
       response.unspent.Neo.should.be.an('array');
       response.unspent.Gas.should.be.an('array');
       done();
