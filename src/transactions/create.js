e983db161c6a77590862d5e899069aac4f74b5cf
Yak Jun Xiang <snowypowers@gmail.com>
2017-10-05 17:00:56 +0800

    clean up create
    add documentation and defaults

--- src/transactions/create.js
+++ src/transactions/create.js
@@ -4,7 +4,9 @@ import { buildScript } from '../sc/scriptBuilder.js'
 export const CURRENT_VERSION = 0
 const ASSETS = {
   'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b': 'NEO',
-  '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7': 'GAS'
+  '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7': 'GAS',
+  'GAS': '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7',
+  'NEO': 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b'
 }
 
 /**
@@ -43,7 +45,7 @@ export const claimTx = (publicKey, claimData, override = {}) => {
  * @param {Object} [override={}] - Optional overrides (eg.custom versions)
  * @return {Transaction} Unsigned Transaction
  */
-export const ContractTx = (publicKey, balances, intents, override) => {
+export const ContractTx = (publicKey, balances, intents, override = {}) => {
   const tx = Object.assign({
     type: 128,
     version: CURRENT_VERSION,
@@ -64,7 +66,7 @@ export const ContractTx = (publicKey, balances, intents, override) => {
  * @param {Object} [override={}] - Optional overrides (eg.custom versions)
  * @return {string} Unsigned Transaction
  */
-export const invocationTx = (publicKey, balances, intents, invoke, gasCost, override) => {
+export const invocationTx = (publicKey, balances, intents, invoke, gasCost, override = {}) => {
   const tx = Object.assign({
     type: 209,
     version: CURRENT_VERSION,
@@ -76,20 +78,26 @@ export const invocationTx = (publicKey, balances, intents, invoke, gasCost, over
   return Object.assign(tx, { inputs, attributes, outputs: intents.concat(change), script, gas: gasCost }, override)
 }
 
+/**
+ * Calculate the inputs required given the intents and gasCost. gasCost has to be seperate because it will not be reflected as an TransactionOutput.
+ * @param {string} publicKey
+ * @param {Balance} balances - Balance of all assets available.
+ * @param {TransactionOutput[]} intents - All sending intents
+ * @param {number} gasCost - gasCost required for the transaction.
+ * @return {Object} {inputs: TransactionInput[], change: TransactionOutput[] }
+ */
 const calculateInputs = (publicKey, balances, intents, gasCost = 0) => {
-  // We will work in Fixed8 notation here
+  // We will work in integers here to be more accurate.
+  // As assets are stored as Fixed8, we just multiple everything by 10e8 and round off to get integers.
   const requiredAssets = intents.reduce((assets, intent) => {
     const fixed8Value = Math.floor(intent.value * 100000000)
-    if (assets[intent.assetId]) {
-      assets[intent.assetId] += fixed8Value
-    } else {
-      assets[intent.assetId] = fixed8Value
-    }
+    assets[intent.assetId] ? assets[intent.assetId] += fixed8Value : assets[intent.assetId] = fixed8Value
     return assets
   }, {})
   // Add GAS cost in
   if (gasCost > 0) {
-    requiredAssets['602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'] ? requiredAssets['602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'] += gasCost * 100000000 : requiredAssets['602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'] = gasCost * 100000000
+    const fixed8GasCost = gasCost * 100000000
+    requiredAssets[ASSETS.GAS] ? requiredAssets[ASSETS.GAS] += fixed8GasCost : requiredAssets[ASSETS.GAS] = fixed8GasCost
   }
   let change = []
   const inputs = Object.keys(requiredAssets).map((assetId) => {
