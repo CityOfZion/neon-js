14f18d764b726047f1f802dddf599fd088b5c389
Yak Jun Xiang <snowypowers@gmail.com>
2017-10-07 16:13:39 +0800

    Core methods for manipulating keys

---
+++ src/wallet/core.js
@@ -0,0 +1,85 @@
+/**
+ * Core methods for manipulating keys
+ * NEP2 <=> WIF <=> Private => Public => ScriptHash <=> Address
+ * Keys are arranged in order of derivation.
+ * Arrows determine the direction.
+ *
+ * Methods are named as get<This>From<That> where:
+ * <This> is lower on the chain (on the right)
+ * <That> is higher on the chain (on the left)
+ *
+ * NEP2 Methods are found within NEP2 standard.
+ * All Methods take in Big-Endian strings and return Big-Endian strings.
+ */
+import WIF from 'wif'
+import ecurve from 'ecurve'
+import BigInteger from 'bigi'
+import { hexstring2ab, ab2hexstring, reverseHex, base58, hash160, hash256 } from '../utils'
+
+const ADDR_VERSION = '17'
+
+/**
+ * @param {string} wif
+ * @return {string}
+ */
+export const getPrivateKeyFromWIF = (wif) => {
+  return ab2hexstring(WIF.decode(wif, 128).privateKey)
+}
+
+/**
+ * @param {string} privateKey
+ * @return {string}
+ */
+export const getWIFFromPrivateKey = (privateKey) => {
+  return WIF.encode(128, Buffer.from(privateKey, 'hex'), true)
+}
+
+/**
+ * @param {string} privateKey
+ * @param {boolean} encode
+ * @return {string}
+ */
+export const getPublicKeyFromPrivateKey = (privateKey, encode = true) => {
+  let ecparams = ecurve.getCurveByName('secp256r1')
+  let curvePt = ecparams.G.multiply(BigInteger.fromBuffer(hexstring2ab(privateKey)))
+  return ab2hexstring(curvePt.getEncoded(encode))
+}
+
+/**
+ * VerificationScript serves a very niche purpose.
+ * It is attached as part of the signature when signing a transaction.
+ * Thus, the name 'scriptHash' instead of 'keyHash' is because we are hashing the verificationScript and not the PublicKey.
+ * @param {string} publicKey
+ * @return {string}
+ */
+export const getVerificationScriptFromPublicKey = (publicKey) => {
+  return '21' + publicKey + 'ac'
+}
+
+/**
+ * @param {string} publicKey
+ * @return {string}
+ */
+export const getScriptHashFromPublicKey = (publicKey) => {
+  const verificationScript = getVerificationScriptFromPublicKey(publicKey)
+  return reverseHex(hash160(verificationScript))
+}
+
+/**
+ * @param {string} scriptHash
+ * @return {string}
+ */
+export const getAddressFromScriptHash = (scriptHash) => {
+  scriptHash = reverseHex(scriptHash)
+  const shaChecksum = hash256(ADDR_VERSION + scriptHash).substr(0, 8)
+  return base58.encode(Buffer.from(ADDR_VERSION + scriptHash + shaChecksum, 'hex'))
+}
+
+/**
+ * @param {string} address
+ * @return {string}
+ */
+export const getScriptHashFromAddress = (address) => {
+  let hash = ab2hexstring(base58.decode(address))
+  return reverseHex(hash.substr(2, 40))
+}
