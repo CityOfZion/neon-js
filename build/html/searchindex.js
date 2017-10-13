50c8f929a8638ef44c96382e7cb08e44b3c4470c
Ethan Fast <Ejhfast@gmail.com>
2017-10-13 09:43:37 -0700

    read the docs (try again with matching version)

---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to neon-js&#8217;s documentation!"],titleterms:{document:0,indic:0,neon:0,tabl:0,welcom:0}})
