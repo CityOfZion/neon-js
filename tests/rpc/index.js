87512a28ed33f31d84ec98b327d9356c92015e60
Ethan Fast <Ejhfast@gmail.com>
2017-10-28 22:05:14 -0700

    missing files for last commit

---
+++ tests/rpc/index.js
@@ -0,0 +1,21 @@
+import Neon from '../../src/index.js'
+
+const client = Neon.create.rpcClient('TestNet')
+
+describe.only('RPC methods', function () {
+  this.timeout(15000)
+
+  it('test block height', (done) => {
+    client.getBlockCount().then((count) => {
+      count.should.be.a('number')
+      done()
+    })
+  })
+
+  it('test storage', (done) => {
+    client.getStorage(Neon.CONST.CONTRACTS.TEST_RPX, "totalSupply").then((response) => {
+      response.should.be.a('string')
+      done()
+    })
+  })
+})
