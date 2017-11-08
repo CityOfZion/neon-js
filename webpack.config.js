22b63dd5b225bf7e43b6482cb036cbf7ec15d991
Yak Jun Xiang <snowypowers@gmail.com>
2017-11-08 11:40:24 +0800

    update webpack build for dev and prod
    Add a un-minified dev for fast build and remove source maps for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const ZopfliPlugin = require('zopfli-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-const common = {
+let common = {
   entry: './src/index.js',
-  devtool: 'source-map',
   module: {
     loaders: [
       {
@@ -26,42 +25,50 @@ const common = {
     'child_process': 'empty'
   },
   plugins: [
-    new UglifyJSPlugin({
-      parallel: true,
-      sourceMap: true
+    new CleanWebpackPlugin(['lib'])
+  ]
+}
+
+module.exports = function (env) {
+  if (env.prod) {
+    common.plugins = common.plugins.concat([
+      new UglifyJSPlugin({
+        parallel: true,
+        sourceMap: true
+      }),
+      new ZopfliPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'zopfli',
+        test: /\.(js|html)$/,
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      })
+    ])
+  } else {
+    common.devtool = 'source-map'
+  }
+  return [
+    Object.assign({}, common, {
+      target: 'node',
+      output: {
+        path: __dirname,
+        filename: './lib/index.js',
+        libraryTarget: 'umd'
+      }
     }),
-    new BundleAnalyzerPlugin(),
-    new ZopfliPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'zopfli',
-      test: /\.(js|html)$/,
-      threshold: 10240,
-      minRatio: 0.8
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+    Object.assign({}, common, {
+      target: 'web',
+      output: {
+        path: __dirname,
+        filename: './lib/browser.js',
+        libraryTarget: 'umd',
+        library: 'Neon' // This is the var name in browser
       }
     })
   ]
 }
-
-module.exports = [
-  Object.assign({}, common, {
-    target: 'node',
-    output: {
-      path: __dirname,
-      filename: './lib/index.js',
-      libraryTarget: 'umd'
-    }
-  }),
-  Object.assign({}, common, {
-    target: 'web',
-    output: {
-      path: __dirname,
-      filename: './lib/browser.js',
-      libraryTarget: 'umd',
-      library: 'Neon' // This is the var name in browser
-    }
-  })
-]
