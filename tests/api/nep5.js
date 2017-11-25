965debf8972b34e84f4658530a8af38d35282fed
David Schwartz <dbschwartz@protonmail.com>
2017-11-24 16:00:31 -0800

    add transfer tokens method to neon-js

--- tests/api/nep5.js
+++ tests/api/nep5.js
@@ -1,14 +1,18 @@
 import * as NEP5 from '../../src/api/nep5'
+import testKeys from '../testKeys.json'
 
 describe('NEP5', function () {
   this.timeout(10000)
+  const net = 'http://seed3.neo.org:20332'
+  const scriptHash = 'd7678dd97c000be3f33e9362e673101bac4ca654'
+
   it('get basic info', () => {
-    return NEP5.getTokenInfo('http://test1.cityofzion.io:8880', '5b7074e873973a6ed3708862f219a6fbf4d1c411')
+    return NEP5.getTokenInfo(net, scriptHash)
       .then(result => {
-        result.name.should.equal('Red Pulse Token 3.1.4')
-        result.symbol.should.equal('RPX')
+        result.name.should.equal('LOCALTOKEN')
+        result.symbol.should.equal('LWTF')
         result.decimals.should.equal(8)
-        result.totalSupply.should.be.above(1000)
+        result.totalSupply.should.equal(1969000)
       })
       .catch((e) => {
         console.log(e)
@@ -16,9 +20,23 @@ describe('NEP5', function () {
       })
   })
   it('get balance', () => {
-    return NEP5.getTokenBalance('http://test1.cityofzion.io:8880', '5b7074e873973a6ed3708862f219a6fbf4d1c411', 'AVf4UGKevVrMR1j3UkPsuoYKSC4ocoAkKx')
+    return NEP5.getTokenBalance(net, scriptHash, testKeys.c.address)
       .then(result => {
         result.should.be.above(0)
+      })
+      .catch((e) => {
+        console.log(e)
+        throw e
+      })
+  })
+  it('transfers tokens', () => {
+    const testNet = 'TestNet'
+    const fromWif = 'L5FzBMGSG2d7HVJL5vWuXfxUKsrkX5irFhtw1L5zU4NAvNuXzd8a'
+    const transferAmount = 1
+    const gasCost = 0
+    return NEP5.doTransferToken(testNet, scriptHash, fromWif, testKeys.c.address, transferAmount, gasCost)
+      .then(({ result }) => {
+        result.should.equal(true)
       })
       .catch((e) => {
         console.log(e)
