45770f7f72d1814283cb69cdac21b8cf83b30a0b
David Schwartz <dbschwartz@protonmail.com>
2017-11-24 16:00:31 -0800

    add transfer tokens method to neon-js

--- src/api/nep5.js
+++ src/api/nep5.js
@@ -1,7 +1,10 @@
 import { ScriptBuilder } from '../sc'
-import { getScriptHashFromAddress } from '../wallet'
+import { getScriptHashFromAddress, Account } from '../wallet'
 import { Query, VMExtractor } from '../rpc'
 import { ab2str, hexstring2ab, reverseHex, fixed82num } from '../utils'
+import { getRPCEndpoint, getBalance } from './neonDB'
+import { Transaction } from '../transactions'
+import { ASSET_ID } from '../consts'
 
 /**
  * Queries for NEP5 Token information.
@@ -42,5 +45,51 @@ export const getTokenBalance = (net, scriptHash, address) => {
   return Query.invokeScript(script, false).execute(net)
     .then((res) => {
       return fixed82num(res.result.stack[0].value)
+    })
+}
+
+/**
+ * Transfers NEP5 Tokens.
+ * @param {string} net
+ * @param {string} scriptHash
+ * @param {string} fromWif
+ * @param {string} toAddress
+ * @param {number} transferAmount
+ * @param {number} gasCost
+ * @param {function} signingFunction
+ * @return {Promise<Response>} RPC response
+ */
+export const doTransferToken = (net, scriptHash, fromWif, toAddress, transferAmount, gasCost = 0, signingFunction = null) => {
+  const account = new Account(fromWif)
+  const rpcEndpointPromise = getRPCEndpoint(net)
+  const balancePromise = getBalance(net, account.address)
+  let signedTx
+  let endpt
+  return Promise.all([rpcEndpointPromise, balancePromise])
+    .then((values) => {
+      endpt = values[0]
+      const balances = values[1]
+      const fromAddrScriptHash = reverseHex(getScriptHashFromAddress(account.address))
+      const intents = [
+        { assetId: ASSET_ID.GAS, value: 0.1, scriptHash: fromAddrScriptHash }
+      ]
+      const toAddrScriptHash = reverseHex(getScriptHashFromAddress(toAddress))
+      const invoke = { scriptHash, operation: 'transfer', args: [fromAddrScriptHash, toAddrScriptHash, transferAmount] }
+      const unsignedTx = Transaction.createInvocationTx(balances, intents, invoke, gasCost, { version: 1 })
+      if (signingFunction) {
+        return signingFunction(unsignedTx, account.publicKey)
+      } else {
+        return unsignedTx.sign(account.privateKey)
+      }
+    })
+    .then((signedResult) => {
+      signedTx = signedResult
+      return Query.sendRawTransaction(signedTx).execute(endpt)
+    })
+    .then((res) => {
+      if (res.result === true) {
+        res.txid = signedTx
+      }
+      return res
     })
 }
--- tests/api/nep5.js
+++ tests/api/nep5.js
@@ -1,9 +1,13 @@
 import * as NEP5 from '../../src/api/nep5'
+import testKeys from '../testKeys.json'
 
 describe('NEP5', function () {
   this.timeout(10000)
+  const net = 'http://test1.cityofzion.io:8880'
+  const rpxScriptHash = '5b7074e873973a6ed3708862f219a6fbf4d1c411'
+
   it('get basic info', () => {
-    return NEP5.getTokenInfo('http://test1.cityofzion.io:8880', '5b7074e873973a6ed3708862f219a6fbf4d1c411')
+    return NEP5.getTokenInfo(net, rpxScriptHash)
       .then(result => {
         result.name.should.equal('Red Pulse Token 3.1.4')
         result.symbol.should.equal('RPX')
@@ -16,9 +20,24 @@ describe('NEP5', function () {
       })
   })
   it('get balance', () => {
-    return NEP5.getTokenBalance('http://test1.cityofzion.io:8880', '5b7074e873973a6ed3708862f219a6fbf4d1c411', 'AVf4UGKevVrMR1j3UkPsuoYKSC4ocoAkKx')
+    return NEP5.getTokenBalance(net, rpxScriptHash, testKeys.c.address)
       .then(result => {
         result.should.be.above(0)
+      })
+      .catch((e) => {
+        console.log(e)
+        throw e
+      })
+  })
+  it('transfers tokens', () => {
+    const testNet = 'TestNet'
+    const scriptHash = 'd7678dd97c000be3f33e9362e673101bac4ca654'
+    const fromWif = 'L5FzBMGSG2d7HVJL5vWuXfxUKsrkX5irFhtw1L5zU4NAvNuXzd8a'
+    const transferAmount = 1
+    const gasCost = 0
+    return NEP5.doTransferToken(testNet, scriptHash, fromWif, testKeys.c.address, transferAmount, gasCost)
+      .then(({ result }) => {
+        result.should.equal(true)
       })
       .catch((e) => {
         console.log(e)
