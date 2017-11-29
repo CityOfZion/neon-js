55ab80b898bb9f7dc63b0085e3e98a1c236f2c08
Yak Jun Xiang <snowypowers@gmail.com>
2017-11-29 15:18:40 +0800

    add version prop

--- src/wallet/Wallet.js
+++ src/wallet/Wallet.js
@@ -24,9 +24,11 @@ import { DEFAULT_SCRYPT, DEFAULT_WALLET } from '../consts'
  * @param {object} config.extra - Extra information of wallet.
  */
 class Wallet {
-  constructor ({ name = 'myWallet', scrypt = DEFAULT_SCRYPT, accounts = [], extra = null } = DEFAULT_WALLET) {
+  constructor ({ name = 'myWallet', version = '1.0', scrypt = DEFAULT_SCRYPT, accounts = [], extra = null } = DEFAULT_WALLET) {
     /** @type {string} */
     this.name = name
+    /** @type {string} */
+    this.version = version
     /** @type {object} */
     this.scrypt = scrypt
     /** @type {Account[]} */
