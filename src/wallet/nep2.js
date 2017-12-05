a0fe7656f423ef91692b672acbbdc6d83726e68a
Yak Jun Xiang <snowypowers@gmail.com>
2017-12-05 16:09:24 +0800

    add scryptParams docstring for decrypt

--- src/wallet/nep2.js
+++ src/wallet/nep2.js
@@ -72,6 +72,7 @@ export const encrypt = (wifKey, keyphrase, scryptParams = DEFAULT_SCRYPT) => {
  * Decrypts an encrypted key using a given keyphrase under NEP-2 Standard.
  * @param {string} encryptedKey - The encrypted key (58 chars long).
  * @param {string} keyphrase - The password will be encoded as UTF-8 and normalized using Unicode Normalization Form C (NFC).
+ * @param {scryptParams} scryptParams - Parameters for Scrypt. Defaults to NEP2 specified parameters.
  * @returns {string} The decrypted WIF key.
  */
 export const decrypt = (encryptedKey, keyphrase, scryptParams = DEFAULT_SCRYPT) => {
