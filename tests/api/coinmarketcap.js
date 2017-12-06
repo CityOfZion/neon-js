02a247ef7e9c52e929b6384e694453d43b06c12c
Yak Jun Xiang <snowypowers@gmail.com>
2017-12-06 12:31:15 +0800

    Fully mock cmc

--- tests/api/coinmarketcap.js
+++ tests/api/coinmarketcap.js
@@ -1,6 +1,23 @@
 import * as CMC from '../../src/api/coinmarketcap'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import mockData from './mockData.json'
 
 describe('coinmarketcap', function () {
+  let mock
+
+  before(() => {
+    mock = new MockAdapter(axios)
+    mock
+    .onGet(mockData.cmc.gas2usd.url).reply(200, mockData.cmc.gas2usd.response)
+    .onGet(mockData.cmc.neo2sgd.url).reply(200, mockData.cmc.neo2sgd.response)
+    .onGet().reply(200, mockData.cmc.error)
+  })
+
+  after(() => {
+    mock.restore()
+  })
+
   it('get price of GAS in usd', () => {
     return CMC.getPrice('GAS').should.eventually.be.a('number')
   })
@@ -10,10 +27,10 @@ describe('coinmarketcap', function () {
   })
 
   it('rejects Promise when given unknown currency', () => {
-    return CMC.getPrice('NEO', 'wtf').should.eventually.be.rejected
+    return CMC.getPrice('NEO', 'wtf').should.eventually.be.rejectedWith(Error, 'wtf is not one of the accepted currencies!')
   })
 
   it('rejects Promise when given unknown coin', () => {
-    return CMC.getPrice('NEON').should.eventually.be.rejected
+    return CMC.getPrice('NEON').should.eventually.be.rejectedWith(Error, `id not found`)
   })
 })
