508dc19b16b5ec31cbc4158d3e036e606831c70a
Yak Jun Xiang <snowypowers+github@gmail.com>
2017-12-17 22:53:49 -0800

    [Fix] Fix transferToken tests + whitelist published files(#108)
    * cleanup integration tests
    
    * minor cleanup of api/nep5
    
    * update invokeTx tests to use 0 gas
    
    GAS FEE MUST BE INTEGER CURRENTLY
    
    * use package.json to whitelist files to publish to npm

--- tests/integration/api/nep5.js
+++ tests/integration/api/nep5.js
@@ -3,9 +3,12 @@ import { DEFAULT_RPC, CONTRACTS } from '../../../src/consts'
 import testKeys from '../../unit/testKeys.json'
 
 describe('Integration: API NEP5', function () {
+  this.timeout(20000)
+  const scriptHash = CONTRACTS.TEST_LWTF
+
   describe('getToken', function () {
     it('get info and balance', () => {
-      return NEP5.getToken(DEFAULT_RPC.TEST, CONTRACTS.TEST_LWTF, testKeys.c.address)
+      return NEP5.getToken(DEFAULT_RPC.TEST, scriptHash, testKeys.c.address)
         .then(result => {
           result.should.have.keys(['name', 'symbol', 'decimals', 'totalSupply', 'balance'])
           result.name.should.equal('LOCALTOKEN')
@@ -15,5 +18,16 @@ describe('Integration: API NEP5', function () {
           result.balance.should.be.above(0)
         })
     })
+  })
+
+  it('doTransferToken', () => {
+    const testNet = 'TestNet'
+    const transferAmount = 1
+    const gasCost = 0
+    return NEP5.doTransferToken(testNet, scriptHash, testKeys.c.wif, testKeys.b.address, transferAmount, gasCost)
+      .then(({ result, txid }) => {
+        result.should.equal(true)
+        console.log(txid)
+      })
   })
 })
