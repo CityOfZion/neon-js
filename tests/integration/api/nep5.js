7b81b60dcbb44383480b12322dfdeb5dd25e12b8
Yak Jun Xiang <snowypowers@gmail.com>
2018-01-03 20:09:32 -0800

    test(api): Add test for getTokenBalance and getTokenInfo
    add CONTRACTS.TEST_RHTT4

--- tests/integration/api/nep5.js
+++ tests/integration/api/nep5.js
@@ -20,13 +20,30 @@ describe('Integration: API NEP5', function () {
     })
 
     it('should parse 0 decimals correctly', () => {
-      return NEP5.getToken(DEFAULT_RPC.TEST, '400cbed5b41014788d939eaf6286e336e7140f8c')
-      .then(result => {
-        result.should.have.keys(['name', 'symbol', 'decimals', 'totalSupply', 'balance'])
-        result.decimals.should.equal(0)
-        result.totalSupply.should.equal(60000)
-      })
+      return NEP5.getToken(DEFAULT_RPC.TEST, CONTRACTS.TEST_RHTT4, testKeys.a.address)
+        .then(result => {
+          result.should.have.keys(['name', 'symbol', 'decimals', 'totalSupply', 'balance'])
+          result.decimals.should.equal(0)
+          result.totalSupply.should.equal(60000)
+          result.balance.should.equal(2)
+        })
     })
+  })
+
+  it('getTokenInfo', () => {
+    return NEP5.getTokenInfo(DEFAULT_RPC.TEST, CONTRACTS.TEST_RHTT4)
+      .then(({ name, symbol, decimals, totalSupply }) => {
+        name.should.equal('Redeemable HashPuppy Testnet Token 4')
+        symbol.should.equal('RHTT4')
+        decimals.should.equal(0)
+        totalSupply.should.equal(60000)
+      })
+  })
+  it('getTokenBalance', () => {
+    return NEP5.getTokenBalance(DEFAULT_RPC.TEST, CONTRACTS.TEST_RHTT4, testKeys.b.address)
+      .then(result => {
+        result.should.equal(2)
+      })
   })
 
   it.skip('doTransferToken', () => {
