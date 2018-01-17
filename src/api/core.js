0f4d53b56f452c64d25c6eb05b985335145c70ca
David Schwartz <dbschwartz@protonmail.com>
2018-01-16 20:27:00 -0800

    feat(api): full neoscan API and expose set of loadbalanced methods  (#145)
    * add getrpcendpoint
    
    * add methods for neoscan migration
    
    * update neoscan endpoint and other changes
    
    * adding changes to see total gas claims spent and unspent
    
    * changes to reformat and improve getMaxClaimAmount
    
    * fix method name
    
    * expose load balance
    
    * unfreeze switch

--- src/api/core.js
+++ src/api/core.js
@@ -10,18 +10,18 @@ import logger from '../logging'
 const log = logger('api')
 
 /** This determines which API we should dial.
-* 0 means 100% neoscan
-* 1 means 100% neonDB
-* This is ensure that we do not always hit the failing endpoint.
-*/
-var apiSwitch = 1
-var switchFrozen = true
+ * 0 means 100% neoscan
+ * 1 means 100% neonDB
+ * This is ensure that we do not always hit the failing endpoint.
+ */
+let apiSwitch = 0
+let switchFrozen = false
 
 /**
  * Sets the API switch to the provided value
  * @param {number} netSetting - The new value between 0 and 1 inclusive.
  */
-export const setApiSwitch = (newSetting) => {
+export const setApiSwitch = newSetting => {
   if (newSetting >= 0 && newSetting <= 1) apiSwitch = newSetting
 }
 
@@ -30,7 +30,7 @@ export const setApiSwitch = (newSetting) => {
  *  This does not mean that we do not use the other provider. This only means that we will not change our preference for the main provider.
  * @param {bool} newSetting - The new setting for freeze.
  */
-export const setSwitchFreeze = (newSetting) => {
+export const setSwitchFreeze = newSetting => {
   switchFrozen = !!newSetting
   log.info(`core/setSwitchFreeze API switch is frozen: ${switchFrozen}`)
 }
@@ -48,10 +48,10 @@ const increaseNeonDBWeight = () => {
     log.info(`core API Switch increasing weight towards neonDB`)
   }
 }
-const loadBalance = (func, config) => {
+export const loadBalance = (func, config) => {
   if (Math.random() > apiSwitch) {
     return func(config, neoscan)
-      .then((c) => {
+      .then(c => {
         increaseNeoscanWeight()
         return c
       })
@@ -61,7 +61,7 @@ const loadBalance = (func, config) => {
       })
   } else {
     return func(config, neonDB)
-      .then((c) => {
+      .then(c => {
         increaseNeonDBWeight()
         return c
       })
@@ -90,12 +90,13 @@ const loadBalance = (func, config) => {
  */
 export const getBalanceFrom = (config, api) => {
   checkProperty(config, 'net', 'address')
-  if (!api.getBalance || !api.getRPCEndpoint) throw new Error('Invalid type. Is this an API object?')
+  if (!api.getBalance || !api.getRPCEndpoint)
+    throw new Error('Invalid type. Is this an API object?')
   const balanceP = api.getBalance(config.net, config.address)
   const urlP = api.getRPCEndpoint(config.net)
 
   return Promise.all([balanceP, urlP])
-    .then((values) => {
+    .then(values => {
       const override = { balance: values[0] }
       if (!config.url) override.url = values[1]
       return Object.assign(config, override)
@@ -116,14 +117,15 @@ export const getBalanceFrom = (config, api) => {
  */
 export const getClaimsFrom = (config, api) => {
   checkProperty(config, 'net', 'address')
-  if (!api.getBalance || !api.getRPCEndpoint) throw new Error('Invalid type. Is this an API object?')
+  if (!api.getBalance || !api.getRPCEndpoint)
+    throw new Error('Invalid type. Is this an API object?')
   const claimsP = api.getClaims(config.net, config.address)
   // Get URL
   const urlP = api.getRPCEndpoint(config.net)
   // Return {url, balance, ...props}
 
   return Promise.all([claimsP, urlP])
-    .then((values) => {
+    .then(values => {
       return Object.assign(config, { claims: values[0], url: values[1] })
     })
     .catch(err => {
@@ -173,7 +175,7 @@ export const createTx = (config, txType) => {
  * @param {function} [config.signingFunction] - External signing function. Requires publicKey.
  * @return {Promise<object>} Configuration object.
  */
-export const signTx = (config) => {
+export const signTx = config => {
   checkProperty(config, 'tx')
   let promise
   if (config.signingFunction) {
@@ -181,12 +183,17 @@ export const signTx = (config) => {
     promise = config.signingFunction(config.tx, acct.publicKey)
   } else if (config.privateKey) {
     let acct = new Account(config.privateKey)
-    if (config.address !== acct.address) return Promise.reject(new Error('Private Key and Balance address does not match!'))
-    promise = Promise.resolve(config.tx.sign(config.privateKey))
+    if (config.address !== acct.address)
+      return Promise.reject(
+        new Error('Private Key and Balance address does not match!')
+      )
+    promise = Promise.resolve(config.tx.sign(acct.privateKey))
   } else {
-    return Promise.reject(new Error('Needs privateKey or signingFunction to sign!'))
+    return Promise.reject(
+      new Error('Needs privateKey or signingFunction to sign!')
+    )
   }
-  return promise.then((signedTx) => {
+  return promise.then(signedTx => {
     return Object.assign(config, { tx: signedTx })
   })
 }
@@ -198,11 +205,11 @@ export const signTx = (config) => {
  * @param {string} config.url - NEO Node URL.
  * @return {object} Configuration object + response
  */
-export const sendTx = (config) => {
+export const sendTx = config => {
   checkProperty(config, 'tx', 'url')
   return Query.sendRawTransaction(config.tx)
     .execute(config.url)
-    .then((res) => {
+    .then(res => {
       // Parse result
       if (res.result === true) {
         res.txid = config.tx.hash
@@ -211,16 +218,12 @@ export const sendTx = (config) => {
         }
       } else {
         const dump = {
-          net: config.net,
-          address: config.address,
-          intents: config.intents,
-          balance: config.balance,
-          claims: config.claims,
-          script: config.script,
-          gas: config.gas,
-          tx: config.tx
+          net: config.net, address: config.address, intents: config.intents, balance: config.balance, claims: config.claims, script: config.script, gas: config.gas, tx: config.tx
         }
-        log.error(`Transaction failed for ${config.address}: ${config.tx.serialize()}`, dump)
+        log.error(
+          `Transaction failed for ${config.address}: ${config.tx.serialize()}`,
+          dump
+        )
       }
       return Object.assign(config, { response: res })
     })
@@ -236,8 +239,12 @@ export const sendTx = (config) => {
  */
 export const makeIntent = (assetAmts, address) => {
   const acct = new Account(address)
-  return Object.keys(assetAmts).map((key) => {
-    return TransactionOutput({ assetId: ASSET_ID[key], value: assetAmts[key], scriptHash: acct.scriptHash })
+  return Object.keys(assetAmts).map(key => {
+    return TransactionOutput({
+      assetId: ASSET_ID[key],
+      value: assetAmts[key],
+      scriptHash: acct.scriptHash
+    })
   })
 }
 
@@ -246,16 +253,17 @@ export const makeIntent = (assetAmts, address) => {
  * @param {object} config - Configuration object.
  * @param {string} config.net - 'MainNet', 'TestNet' or a neon-wallet-db URL.
  * @param {string} config.address - Wallet address
- * @param {string} [config.privateKey] - private key to sign with. Either this or signingFunction is required.
+ * @param {string} [config.privateKey] - private key to sign with. Either this or signingFunction and public key is required.
  * @param {function} [config.signingFunction] - An external signing function to sign with. Either this or privateKey is required.
+ * @param {string} [config.publicKey] - A public key for the singing function. Either this or privateKey is required.
  * @param {TransactionOutput[]} config.intents - Intents.
  * @return {object} Configuration object.
  */
-export const sendAsset = (config) => {
+export const sendAsset = config => {
   return loadBalance(getBalanceFrom, config)
-    .then((c) => createTx(c, 'contract'))
-    .then((c) => signTx(c))
-    .then((c) => sendTx(c))
+    .then(c => createTx(c, 'contract'))
+    .then(c => signTx(c))
+    .then(c => sendTx(c))
     .catch(err => {
       const dump = {
         net: config.net,
@@ -274,22 +282,19 @@ export const sendAsset = (config) => {
  * @param {object} config - Configuration object.
  * @param {string} config.net - 'MainNet', 'TestNet' or a neon-wallet-db URL.
  * @param {string} config.address - Wallet address
- * @param {string} [config.privateKey] - private key to sign with. Either this or signingFunction is required.
+ * @param {string} [config.privateKey] - private key to sign with. Either this or signingFunction and publicKey is required.
  * @param {function} [config.signingFunction] - An external signing function to sign with. Either this or privateKey is required.
+ * @param {string} [config.publicKey] - A public key for the singing function. Either this or privateKey is required.
  * @return {object} Configuration object.
  */
-export const claimGas = (config) => {
+export const claimGas = config => {
   return loadBalance(getClaimsFrom, config)
-    .then((c) => createTx(c, 'claim'))
-    .then((c) => signTx(c))
-    .then((c) => sendTx(c))
+    .then(c => createTx(c, 'claim'))
+    .then(c => signTx(c))
+    .then(c => sendTx(c))
     .catch(err => {
       const dump = {
-        net: config.net,
-        address: config.address,
-        intents: config.intents,
-        claims: config.claims,
-        tx: config.tx
+        net: config.net, address: config.address, intents: config.intents, claims: config.claims, tx: config.tx
       }
       log.error(`claimGas failed with ${err.message}. Dumping config`, dump)
       throw err
@@ -301,29 +306,24 @@ export const claimGas = (config) => {
  * @param {object} config - Configuration object.
  * @param {string} config.net - 'MainNet', 'TestNet' or a neon-wallet-db URL.
  * @param {string} config.address - Wallet address
- * @param {string} [config.privateKey] - private key to sign with. Either this or signingFunction is required.
+ * @param {string} [config.privateKey] - private key to sign with. Either this or signingFunction and publicKey is required.
  * @param {function} [config.signingFunction] - An external signing function to sign with. Either this or privateKey is required.
+ * @param {string} [config.publicKey] - A public key for the singing function. Either this or privateKey is required.
  * @param {object} [config.intents] - Intents
  * @param {string} config.script - VM script. Must include empty args parameter even if no args are present
  * @param {number} config.gas - gasCost of VM script.
  * @return {object} Configuration object.
  */
-export const doInvoke = (config) => {
+export const doInvoke = config => {
   return loadBalance(getBalanceFrom, config)
-    .then((c) => addAttributesForMintToken(c))
-    .then((c) => createTx(c, 'invocation'))
-    .then((c) => signTx(c))
-    .then((c) => attachInvokedContractForMintToken(c))
-    .then((c) => sendTx(c))
+    .then(c => addAttributesForMintToken(c))
+    .then(c => createTx(c, 'invocation'))
+    .then(c => signTx(c))
+    .then(c => attachInvokedContractForMintToken(c))
+    .then(c => sendTx(c))
     .catch(err => {
       const dump = {
-        net: config.net,
-        address: config.address,
-        intents: config.intents,
-        balance: config.balance,
-        script: config.script,
-        gas: config.gas,
-        tx: config.tx
+        net: config.net, address: config.address, intents: config.intents, balance: config.balance, script: config.script, gas: config.gas, tx: config.tx
       }
       log.error(`doInvoke failed with ${err.message}. Dumping config`, dump)
       throw err
@@ -335,13 +335,19 @@ export const doInvoke = (config) => {
  * @param {object} config - Configuration object.
  * @return {object} Configuration object.
  */
-const addAttributesForMintToken = (config) => {
+const addAttributesForMintToken = config => {
   if (!config.override) config.override = {}
-  if ((typeof config.script === 'object') && config.script.operation === 'mintTokens' && config.script.scriptHash) {
-    config.override.attributes = [{
-      data: reverseHex(config.script.scriptHash),
-      usage: TxAttrUsage.Script
-    }]
+  if (
+    typeof config.script === 'object' &&
+    config.script.operation === 'mintTokens' &&
+    config.script.scriptHash
+  ) {
+    config.override.attributes = [
+      {
+        data: reverseHex(config.script.scriptHash),
+        usage: TxAttrUsage.Script
+      }
+    ]
   }
   return config
 }
@@ -351,10 +357,15 @@ const addAttributesForMintToken = (config) => {
  * @param {object} config - Configuration object.
  * @return {object} Configuration object.
  */
-const attachInvokedContractForMintToken = (config) => {
-  if ((typeof config.script === 'object') && config.script.operation === 'mintTokens' && config.script.scriptHash) {
-    return Query.getContractState(config.script.scriptHash).execute(config.url)
-      .then((contractState) => {
+const attachInvokedContractForMintToken = config => {
+  if (
+    typeof config.script === 'object' &&
+    config.script.operation === 'mintTokens' &&
+    config.script.scriptHash
+  ) {
+    return Query.getContractState(config.script.scriptHash)
+      .execute(config.url)
+      .then(contractState => {
         const attachInvokedContract = {
           invocationScript: '0000',
           verificationScript: contractState.result.script
@@ -377,4 +388,66 @@ const checkProperty = (obj, ...props) => {
       throw new ReferenceError(`Property not found: ${prop}`)
     }
   }
+}
+
+/**
+ * Helper method to returns an appropriate RPC endpoint retrieved from an endpoint.
+ * @param {object} config - Configuration object.
+ * @param {string} config.net - 'MainNet', 'TestNet' or a custom URL.
+ * @param {object} api - The endpoint API object. eg, neonDB or Neoscan.
+ * @return {Promise<string>} - URL
+ */
+export const getRPCEndpointFrom = (config, api) => {
+  checkProperty(config, 'net')
+  if (!api.getRPCEndpoint)
+    throw new Error('Invalid type. Is this an API object?')
+  const { net } = config
+  return api.getRPCEndpoint(net)
+}
+
+/**
+ * Helper method to get transaction history for an account
+ * @param {object} config - Configuration object.
+ * @param {string} config.net - 'MainNet', 'TestNet' or a custom URL.
+ * @param {string} config.address - Wallet address
+ * @param {object} api - The endpoint API object. eg, neonDB or Neoscan.
+ * @return {Promise<string>} - URL
+ */
+export const getTransactionHistoryFrom = (config, api) => {
+  checkProperty(config, 'net', 'address')
+  if (!api.getTransactionHistory)
+    throw new Error('Invalid type. Is this an API object?')
+  const { address, net } = config
+  return api.getTransactionHistory(net, address)
+}
+
+/**
+ * Helper method to get the current height of the light wallet DB
+ * @param {object} config - Configuration object.
+ * @param {string} config.net - 'MainNet', 'TestNet' or a custom URL.
+ * @param {object} api - The endpoint API object. eg, neonDB or Neoscan.
+ * @return {Promise<string>} - URL
+ */
+export const getWalletDBHeightFrom = (config, api) => {
+  checkProperty(config, 'net')
+  if (!api.getWalletDBHeight)
+    throw new Error('Invalid type. Is this an API object?')
+  const { net } = config
+  return api.getWalletDBHeight(net)
+}
+
+/**
+ * Helper method to get the maximum amount of gas claimable after spending all NEO.
+ * @param {object} config - Configuration object.
+ * @param {string} config.net - 'MainNet', 'TestNet'
+ * @param {string} config.address - Wallet address
+ * @param {object} api - The endpoint APi object. eg, neonDB or Neoscan.
+ * @return {object} Configuration object + url + balance
+ */
+export const getMaxClaimAmountFrom = (config, api) => {
+  checkProperty(config, 'net', 'address')
+  if (!api.getMaxClaimAmount)
+    throw new Error('Invalid type. Is this an API object?')
+  const { net, address } = config
+  return api.getMaxClaimAmount(net, address)
 }
--- src/api/neonDB.js
+++ src/api/neonDB.js
@@ -13,7 +13,7 @@ export const name = 'neonDB'
  * @param {string} net - 'MainNet', 'TestNet', or custom neon-wallet-db URL.
  * @return {string} URL of API endpoint.
  */
-export const getAPIEndpoint = (net) => {
+export const getAPIEndpoint = net => {
   switch (net) {
     case 'MainNet':
       return 'http://api.wallet.cityofzion.io'
@@ -31,16 +31,15 @@ export const getAPIEndpoint = (net) => {
  */
 export const getBalance = (net, address) => {
   const apiEndpoint = getAPIEndpoint(net)
-  return axios.get(apiEndpoint + '/v2/address/balance/' + address)
-    .then((res) => {
-      const bal = new Balance({ net, address: res.data.address })
-      Object.keys(res.data).map((key) => {
-        if (key === 'net' || key === 'address') return
-        bal.addAsset(key, res.data[key])
-      })
-      log.info(`Retrieved Balance for ${address} from neonDB ${net}`)
-      return bal
+  return axios.get(apiEndpoint + '/v2/address/balance/' + address).then(res => {
+    const bal = new Balance({ net, address: res.data.address })
+    Object.keys(res.data).map(key => {
+      if (key === 'net' || key === 'address') return
+      bal.addAsset(key, res.data[key])
     })
+    log.info(`Retrieved Balance for ${address} from neonDB ${net}`)
+    return bal
+  })
 }
 
 /**
@@ -51,7 +50,7 @@ export const getBalance = (net, address) => {
  */
 export const getClaims = (net, address) => {
   const apiEndpoint = getAPIEndpoint(net)
-  return axios.get(apiEndpoint + '/v2/address/claims/' + address).then((res) => {
+  return axios.get(apiEndpoint + '/v2/address/claims/' + address).then(res => {
     const claimData = res.data
     claimData.claims = claimData.claims.map(c => {
       return {
@@ -63,7 +62,26 @@ export const getClaims = (net, address) => {
         value: c.value
       }
     })
+    log.info(`Retrieved Claims for ${address} from neonDB ${net}`)
     return new Claims(claimData)
+  })
+}
+
+/**
+ * Gets the maximum amount of gas claimable after spending all NEO.
+ * @param {string} net - 'MainNet' or 'TestNet'.
+ * @param {string} address - Address to check.
+ * @return {Promise<Fixed8>} An object with available and unavailable GAS amounts.
+ */
+export const getMaxClaimAmount = (net, address) => {
+  const apiEndpoint = getAPIEndpoint(net)
+  return axios.get(apiEndpoint + '/v2/address/claims/' + address).then(res => {
+    log.info(
+      `Retrieved maximum amount of gas claimable after spending all NEO for ${address} from neonDB ${net}`
+    )
+    return new Fixed8(res.data.total_claim + res.data.total_unspent_claim).div(
+      100000000
+    )
   })
 }
 
@@ -72,9 +90,9 @@ export const getClaims = (net, address) => {
  * @param {string} net - 'MainNet' or 'TestNet'.
  * @return {Promise<string>} The URL of the best performing node.
  */
-export const getRPCEndpoint = (net) => {
+export const getRPCEndpoint = net => {
   const apiEndpoint = getAPIEndpoint(net)
-  return axios.get(apiEndpoint + '/v2/network/best_node').then((response) => {
+  return axios.get(apiEndpoint + '/v2/network/best_node').then(response => {
     log.info(`Best node from neonDB ${net}: ${response.data.node}`)
     return response.data.node
   })
@@ -88,10 +106,12 @@ export const getRPCEndpoint = (net) => {
  */
 export const getTransactionHistory = (net, address) => {
   const apiEndpoint = getAPIEndpoint(net)
-  return axios.get(apiEndpoint + '/v2/address/history/' + address).then((response) => {
-    log.info(`Retrieved History for ${address} from neonDB ${net}`)
-    return response.data.history
-  })
+  return axios
+    .get(apiEndpoint + '/v2/address/history/' + address)
+    .then(response => {
+      log.info(`Retrieved History for ${address} from neonDB ${net}`)
+      return response.data.history
+    })
 }
 
 /**
@@ -99,9 +119,9 @@ export const getTransactionHistory = (net, address) => {
  * @param {string} net - 'MainNet' or 'TestNet'.
  * @return {Promise<number>} Current height.
  */
-export const getWalletDBHeight = (net) => {
+export const getWalletDBHeight = net => {
   const apiEndpoint = getAPIEndpoint(net)
-  return axios.get(apiEndpoint + '/v2/block/height').then((response) => {
+  return axios.get(apiEndpoint + '/v2/block/height').then(response => {
     return parseInt(response.data.block_height)
   })
 }
@@ -121,7 +141,7 @@ export const doClaimAllGas = (net, privateKey, signingFunction) => {
   let signedTx // Scope this outside so that all promises have this
   let endpt
   return Promise.all([rpcEndpointPromise, claimsPromise])
-    .then((values) => {
+    .then(values => {
       endpt = values[0]
       const claims = values[1]
       if (claims.length === 0) throw new Error('No claimable gas!')
@@ -132,11 +152,11 @@ export const doClaimAllGas = (net, privateKey, signingFunction) => {
         return unsignedTx.sign(account.privateKey)
       }
     })
-    .then((signedResult) => {
+    .then(signedResult => {
       signedTx = signedResult
       return Query.sendRawTransaction(signedTx).execute(endpt)
     })
-    .then((res) => {
+    .then(res => {
       if (res.result === true) {
         res.txid = signedTx
       } else {
@@ -158,20 +178,24 @@ export const doClaimAllGas = (net, privateKey, signingFunction) => {
 export const doMintTokens = (net, scriptHash, fromWif, neo, gasCost, signingFunction) => {
   log.warn('doMintTokens will be deprecated in favor of doInvoke')
   const account = new Account(fromWif)
-  const intents = [{ assetId: ASSET_ID.NEO, value: neo, scriptHash: scriptHash }]
+  const intents = [
+    { assetId: ASSET_ID.NEO, value: neo, scriptHash: scriptHash }
+  ]
   const invoke = { operation: 'mintTokens', scriptHash, args: [] }
   const rpcEndpointPromise = getRPCEndpoint(net)
   const balancePromise = getBalance(net, account.address)
   let signedTx
   let endpt
   return Promise.all([rpcEndpointPromise, balancePromise])
-    .then((values) => {
+    .then(values => {
       endpt = values[0]
       let balances = values[1]
-      const attributes = [{
-        data: reverseHex(scriptHash),
-        usage: TxAttrUsage.Script
-      }]
+      const attributes = [
+        {
+          data: reverseHex(scriptHash),
+          usage: TxAttrUsage.Script
+        }
+      ]
       const unsignedTx = Transaction.createInvocationTx(balances, intents, invoke, gasCost, { attributes })
       if (signingFunction) {
         return signingFunction(unsignedTx, account.publicKey)
@@ -179,11 +203,11 @@ export const doMintTokens = (net, scriptHash, fromWif, neo, gasCost, signingFunc
         return unsignedTx.sign(account.privateKey)
       }
     })
-    .then((signedResult) => {
+    .then(signedResult => {
       signedTx = signedResult
       return Query.getContractState(scriptHash).execute(endpt)
     })
-    .then((contractState) => {
+    .then(contractState => {
       const attachInvokedContract = {
         invocationScript: '0000',
         verificationScript: contractState.result.script
@@ -191,7 +215,7 @@ export const doMintTokens = (net, scriptHash, fromWif, neo, gasCost, signingFunc
       signedTx.scripts.unshift(attachInvokedContract)
       return Query.sendRawTransaction(signedTx).execute(endpt)
     })
-    .then((res) => {
+    .then(res => {
       if (res.result === true) {
         res.txid = signedTx.hash
       } else {
@@ -215,13 +239,17 @@ export const doSendAsset = (net, toAddress, from, assetAmounts, signingFunction)
   const toAcct = new Account(toAddress)
   const rpcEndpointPromise = getRPCEndpoint(net)
   const balancePromise = getBalance(net, fromAcct.address)
-  const intents = Object.keys(assetAmounts).map((key) => {
-    return { assetId: ASSET_ID[key], value: assetAmounts[key], scriptHash: toAcct.scriptHash }
+  const intents = Object.keys(assetAmounts).map(key => {
+    return {
+      assetId: ASSET_ID[key],
+      value: assetAmounts[key],
+      scriptHash: toAcct.scriptHash
+    }
   })
   let signedTx
   let endpt
   return Promise.all([rpcEndpointPromise, balancePromise])
-    .then((values) => {
+    .then(values => {
       endpt = values[0]
       const balance = values[1]
       const unsignedTx = Transaction.createContractTx(balance, intents)
@@ -231,11 +259,11 @@ export const doSendAsset = (net, toAddress, from, assetAmounts, signingFunction)
         return unsignedTx.sign(fromAcct.privateKey)
       }
     })
-    .then((signedResult) => {
+    .then(signedResult => {
       signedTx = signedResult
       return Query.sendRawTransaction(signedTx).execute(endpt)
     })
-    .then((res) => {
+    .then(res => {
       if (res.result === true) {
         res.txid = signedTx.hash
       } else {
