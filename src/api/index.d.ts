9927ef97c61b6fa0d338e76c5cce93025bc793e9
Yak Jun Xiang <snowypowers@gmail.com>
2018-02-11 17:42:48 -0800

    fix(typings): Run through and fix all typings
    removed module code
    
    moved all semantic typings to top level index file

--- src/api/index.d.ts
+++ src/api/index.d.ts
@@ -1,173 +1,140 @@
-///<reference path="../rpc/index.d.ts" />
-///<reference path="../transactions/index.d.ts" />
+import { Balance, Claims } from '../wallet'
+import { Transaction, TransactionOutput } from '../transactions'
+import { RPCResponse } from '../rpc'
+import { Fixed8 } from '../utils'
 
-declare module '@cityofzion/neon-js' {
-  interface apiConfig {
-    net: string,
-    address: string,
-    privateKey?: string,
-    publicKey?: string,
-    url?: string,
-    balance?: Balance
-  }
-
-  interface AssetAmounts {
-    GAS?: number
-    NEO?: number
-  }
-
-  interface History {
-    address: string
-    history: PastTransaction[]
-    name: string
-    net: 'MainNet' | 'TestNet'
-  }
-
-  interface PastTransaction {
-    GAS: number
-    NEO: number
-    block_index: number
-    gas_sent: boolean
-    neo_sent: boolean
-    txid: string
-  }
-
-  interface Prices {
-    [key: string]: number
-  }
-
-  export module api {
-    //coinmarketcap
-    export namespace cmc {
-      export function getPrice(coin?: string, currency?: string): Promise<number>
-      export function getPrices(coin?: string[], currency?: string): Promise<Prices>
-    }
-
-
-    //core
-    export function getBalanceFrom(config: apiConfig, api: object): apiConfig
-    export function getClaimsFrom(config: apiConfig, api: object): apiConfig
-    export function getRPCEndpointFrom(config: apiConfig, api: object): apiConfig
-    export function getTransactionHistoryFrom(config: apiConfig, api: object): apiConfig
-    export function getWalletDBHeightFrom(config: apiConfig, api: object): apiConfig
-    export function getMaxClaimAmountFrom(config: apiConfig, api: object): apiConfig
-    export function createTx(config: apiConfig, txType: string): apiConfig
-    export function signTx(config: apiConfig): apiConfig
-    export function sendTx(config: apiConfig): apiConfig
-    export function makeIntent(assetAmts: AssetAmounts, address: string): TransactionOutput[]
-    export function sendAsset(config: apiConfig): apiConfig
-    export function claimGas(config: apiConfig): apiConfig
-    export function doInvoke(config: apiConfig): apiConfig
-
-    //neonDB
-    export namespace neonDB {
-      export function getAPIEndpoint(net: string): string
-      export function getBalance(net: string, address: string): Promise<Balance>
-      export function getClaims(net: string, address: string): Promise<Claims>
-      export function getRPCEndpoint(net: string): Promise<string>
-      export function getTransactionHistory(net: string, address: string): Promise<History>
-      export function getWalletDBHeight(net: string): Promise<number>
-
-      export function doClaimAllGas(
-        net: string,
-        privateKey: string
-      ): Promise<RPCResponse>
-      export function doClaimAllGas(
-        net: string,
-        publicKey: string,
-        signingFunction: (unsigned: Transaction, publicKey: string) => Transaction
-      ): Promise<RPCResponse>
-
-      export function doMintTokens(
-        net: string,
-        scriptHash: string,
-        fromWif: string,
-        neo: number,
-        gasCost: number
-      ): Promise<RPCResponse>
-      export function doMintTokens(
-        net: string,
-        scriptHash: string,
-        publicKey: string,
-        neo: number,
-        gasCost: number,
-        signingFunction: (unsigned: Transaction, publicKey: string) => Transaction
-      ): Promise<RPCResponse>
-
-      export function doSendAsset(
-        net: string,
-        toAddress: string,
-        from: string,
-        assetAmounts: AssetAmounts
-      ): Promise<RPCResponse>
-      export function doSendAsset(
-        net: string,
-        toAddress: string,
-        publicKey: string,
-        assetAmounts: AssetAmounts,
-        signingFunction: (unsigned: Transaction, publicKey: string) => Transaction
-      ): Promise<RPCResponse>
-    }
-
-    //neoscan
-    export namespace neoscan {
-      export function getAPIEndpoint(net: string): string
-      export function getRPCEndpoint(net: string): Promise<string>
-      export function getBalance(net: string, address: string): Promise<Balance>
-      export function getClaims(net: string, address: string): Promise<Claims>
-    }
-
-    //nep5
-    export namespace nep5 {
-      export function getTokenInfo(url: string, scriptHash: string): Promise<{ name: string, symbol: string, decimals: number, totalSupply: number }>
-      export function getTokenBalance(url: string, scriptHash: string, address: string): Promise<number>
-      export function getToken(url: string, scriptHash: string, address?: string): Promise<object>
-      export function doTransferToken(
-        net: string,
-        scriptHash: string,
-        fromWif: string,
-        toAddress: string,
-        transferAmount: number,
-        gasCost?: number,
-        signingFunction?: (unsigned: Transaction, publicKey: string) => Transaction
-      ): Promise<RPCResponse>
-    }
-
-    // switch
-    export function setApiSwitch(newSetting: number): void
-    export function setSwitchFreeze(newSetting: boolean): void
-  }
-  export interface semantic {
-    get: {
-      price: (coin?: string, currency?: string) => Promise<number>
-      prices: (coins?: string[], currency?: string) => Promise<object>
-      balance: (net: string, address: string) => Promise<Balance>
-      claims: (net: string, address: string) => Promise<Claim>
-      transactionHistory: (net: string, address: string) => Promise<History>
-      tokenBalance: (net: string, scriptHash: string) => Promise<{ name: string, symbol: string, decimals: number, totalSupply: number }>
-      tokenInfo: (net: string, scriptHash: string) => Promise<{ name: string, symbol: string, decimals: number, totalSupply: number }>
-    }
-    do: {
-      sendAsset: (
-        net: string,
-        toAddress: string,
-        from: string,
-        assetAmounts: AssetAmounts
-      ) => Promise<RPCResponse>
-      claimAllGas: (
-        net: string,
-        privateKey: string
-      ) => Promise<RPCResponse>
-      mintTokens: (
-        net: string,
-        scriptHash: string,
-        fromWif: string,
-        neo: number,
-        gasCost: number
-      ) => Promise<RPCResponse>
-    }
-    sendAsset: (config: apiConfig) => apiConfig
-    claimGas: (config: apiConfig) => apiConfig
-    doInvoke: (config: apiConfig) => apiConfig
-  }
+interface apiConfig {
+  net: string,
+  address: string,
+  privateKey?: string,
+  publicKey?: string,
+  url?: string,
+  balance?: Balance
 }
+
+interface AssetAmounts {
+  GAS?: number
+  NEO?: number
+}
+
+interface History {
+  address: string
+  history: PastTransaction[]
+  name: string
+  net: 'MainNet' | 'TestNet' | string
+}
+
+interface PastTransaction {
+  GAS: number
+  NEO: number
+  block_index: number
+  gas_sent: boolean
+  neo_sent: boolean
+  txid: string
+}
+
+interface Prices {
+  [key: string]: number
+}
+//coinmarketcap
+export namespace cmc {
+  export function getPrice(coin?: string, currency?: string): Promise<number>
+  export function getPrices(coin?: string[], currency?: string): Promise<Prices>
+}
+
+
+//core
+export function getBalanceFrom(config: apiConfig, api: object): apiConfig
+export function getClaimsFrom(config: apiConfig, api: object): apiConfig
+export function getRPCEndpointFrom(config: apiConfig, api: object): apiConfig
+export function getTransactionHistoryFrom(config: apiConfig, api: object): apiConfig
+export function getWalletDBHeightFrom(config: apiConfig, api: object): apiConfig
+export function getMaxClaimAmountFrom(config: apiConfig, api: object): apiConfig
+export function createTx(config: apiConfig, txType: string): apiConfig
+export function signTx(config: apiConfig): apiConfig
+export function sendTx(config: apiConfig): apiConfig
+export function makeIntent(assetAmts: AssetAmounts, address: string): TransactionOutput[]
+export function sendAsset(config: apiConfig): apiConfig
+export function claimGas(config: apiConfig): apiConfig
+export function doInvoke(config: apiConfig): apiConfig
+
+//neonDB
+export namespace neonDB {
+  export function getAPIEndpoint(net: string): string
+  export function getBalance(net: string, address: string): Promise<Balance>
+  export function getClaims(net: string, address: string): Promise<Claims>
+  export function getRPCEndpoint(net: string): Promise<string>
+  export function getTransactionHistory(net: string, address: string): Promise<History>
+  export function getWalletDBHeight(net: string): Promise<number>
+
+  export function doClaimAllGas(
+    net: string,
+    privateKey: string
+  ): Promise<RPCResponse>
+  export function doClaimAllGas(
+    net: string,
+    publicKey: string,
+    signingFunction: (unsigned: Transaction, publicKey: string) => Transaction
+  ): Promise<RPCResponse>
+
+  export function doMintTokens(
+    net: string,
+    scriptHash: string,
+    fromWif: string,
+    neo: number,
+    gasCost: number
+  ): Promise<RPCResponse>
+  export function doMintTokens(
+    net: string,
+    scriptHash: string,
+    publicKey: string,
+    neo: number,
+    gasCost: number,
+    signingFunction: (unsigned: Transaction, publicKey: string) => Transaction
+  ): Promise<RPCResponse>
+
+  export function doSendAsset(
+    net: string,
+    toAddress: string,
+    from: string,
+    assetAmounts: AssetAmounts
+  ): Promise<RPCResponse>
+  export function doSendAsset(
+    net: string,
+    toAddress: string,
+    publicKey: string,
+    assetAmounts: AssetAmounts,
+    signingFunction: (unsigned: Transaction, publicKey: string) => Transaction
+  ): Promise<RPCResponse>
+}
+
+//neoscan
+export namespace neoscan {
+  export function getAPIEndpoint(net: string): string
+  export function getRPCEndpoint(net: string): Promise<string>
+  export function getBalance(net: string, address: string): Promise<Balance>
+  export function getClaims(net: string, address: string): Promise<Claims>
+  export function getMaxClaimAmount(net: string, address: string): Promise<Fixed8>
+  export function getWalletDBHeight(net: string): Promise<number>
+  export function getTransactionHistory(net: string, address: string): Promise<History>
+}
+
+//nep5
+export namespace nep5 {
+  export function getTokenInfo(url: string, scriptHash: string): Promise<{ name: string, symbol: string, decimals: number, totalSupply: number }>
+  export function getTokenBalance(url: string, scriptHash: string, address: string): Promise<number>
+  export function getToken(url: string, scriptHash: string, address?: string): Promise<object>
+  export function doTransferToken(
+    net: string,
+    scriptHash: string,
+    fromWif: string,
+    toAddress: string,
+    transferAmount: number,
+    gasCost?: number,
+    signingFunction?: (unsigned: Transaction, publicKey: string) => Transaction
+  ): Promise<RPCResponse>
+}
+
+// switch
+export function setApiSwitch(newSetting: number): void
+export function setSwitchFreeze(newSetting: boolean): void
--- src/sc/index.d.ts
+++ src/sc/index.d.ts
@@ -1,83 +1,69 @@
-///<reference path="../utils.d.ts" />
+import { StringStream } from '../utils'
 
-declare module '@cityofzion/neon-js' {
-
-  export interface scriptParams {
-    scriptHash: string,
-    operation?: string,
-    args?: Array<any> | string | number | boolean,
-    useTailCall?: boolean
-  }
-
-  export module sc {
-    //ContractParam
-    export class ContractParam {
-      constructor(type: string, value: any)
-
-      static string(value: string): ContractParam
-      static boolean(value: any): ContractParam
-      static integer(value: any): ContractParam
-      static byteArray(value: any, format: string): ContractParam
-      static array(param: any[]): ContractParam
-    }
-
-    //opCode
-    export enum OpCode { }
-
-    //ScriptBuilder
-    export class ScriptBuilder extends u.StringStream {
-      constructor()
-
-      _emitAppCall(scriptHash: string, useTailCall?: boolean): this
-      _emitArray(arr: Array<any>): this
-      _emitString(hexstring: string): this
-      _emitNum(num: number): this
-      _emitParam(param: ContractParam): this
-
-      emit(op: OpCode, args: string): this
-      emitAppCall(
-        scriptHash: string,
-        operation: string,
-        args: Array<any> | string | number | boolean,
-        useTailCall?: boolean): this
-      emitSysCall(api: string): this
-      emitPush(data: Array<any> | string | number | boolean): this
-    }
-
-    export function createScript({ scriptHash, operation, args, useTailCall }: scriptParams): string
-
-    //index
-    interface DeployScriptConfig {
-      script: string,
-      name: string,
-      version: string,
-      author: string,
-      email: string,
-      description: string,
-      needsStorage?: boolean,
-      returnType?: string,
-      paramaterList?: string
-    }
-
-    export function generateDeployScript({
-      script,
-      name,
-      version,
-      author,
-      email,
-      description,
-      needsStorage,
-      returnType,
-      paramaterList
-      }: DeployScriptConfig): string
-  }
-
-  export interface semantic {
-    create: {
-      contractParam: (args: any) => sc.ContractParam
-      script: ({ scriptHash, operation, args, useTailCall }: scriptParams) => string
-      scriptBuilder: (args: any) => sc.ScriptBuilder
-      deployScript: (any) => string
-    }
-  }
+export interface scriptParams {
+  scriptHash: string,
+  operation?: string,
+  args?: Array<any> | string | number | boolean,
+  useTailCall?: boolean
 }
+
+//ContractParam
+export class ContractParam {
+  constructor(type: string, value: any)
+
+  static string(value: string): ContractParam
+  static boolean(value: any): ContractParam
+  static integer(value: any): ContractParam
+  static byteArray(value: any, format: string): ContractParam
+  static array(param: any[]): ContractParam
+}
+
+//opCode
+export enum OpCode { }
+
+//ScriptBuilder
+export class ScriptBuilder extends StringStream {
+  constructor()
+
+  _emitAppCall(scriptHash: string, useTailCall?: boolean): this
+  _emitArray(arr: Array<any>): this
+  _emitString(hexstring: string): this
+  _emitNum(num: number): this
+  _emitParam(param: ContractParam): this
+
+  emit(op: OpCode, args: string): this
+  emitAppCall(
+    scriptHash: string,
+    operation: string,
+    args: Array<any> | string | number | boolean,
+    useTailCall?: boolean): this
+  emitSysCall(api: string): this
+  emitPush(data: Array<any> | string | number | boolean): this
+}
+
+export function createScript({ scriptHash, operation, args, useTailCall }: scriptParams): string
+
+//index
+interface DeployScriptConfig {
+  script: string,
+  name: string,
+  version: string,
+  author: string,
+  email: string,
+  description: string,
+  needsStorage?: boolean,
+  returnType?: string,
+  paramaterList?: string
+}
+
+export function generateDeployScript({
+  script,
+  name,
+  version,
+  author,
+  email,
+  description,
+  needsStorage,
+  returnType,
+  paramaterList
+}: DeployScriptConfig): string
