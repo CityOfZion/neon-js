7dbf86d36aa9736d2d7f22e4cc94b7cc9e296461
Jeroen Peeters <jeroen.peeters@evance.be>
2018-05-22 10:22:50 +0200

    feat(app): init deserialize

---
+++ src/sc/Deserialize.js
@@ -0,0 +1,93 @@
+import StackItemType from './StackItemType'
+
+const Deserialize = serializedArray => {
+  // Split into bytes of 2 characters
+  const byteArray = serializedArray.match(/.{2}/g)
+
+  // Initialize under root byte
+  let iterator = -1
+  let iteratedByte
+
+  const setIteratedByte = () => {
+    iterator++
+    iteratedByte = byteArray[iterator]
+  }
+
+  const stackItemIterator = () => {
+    setIteratedByte()
+    const _result = setResultBase(iteratedByte)
+    const hasChildren = determineChildren(_result.type)
+    const length = intIterator()
+
+    for (let i = 0; i < length; i++) {
+      if (hasChildren) {
+        if (_result.type === 'Array' || _result.type === 'Struct') {
+          _result.value.push(stackItemIterator())
+        }
+      } else if (!hasChildren) {
+        if (_result.type === 'Boolean') {
+          setIteratedByte()
+          _result.value = parseInt(iteratedByte, 16) === 1 // if 1 => true, else => false
+        // } else if (_result.type === 'Integer') {
+        //   setIteratedByte()
+        //   _result.value += parseInt(iteratedByte, 16)
+        } else {
+          _result.value += setIteratedByte()
+        }
+      }
+    }
+  }
+
+  const intIterator = () => {
+    const baseLength = setIteratedByte()
+    if (baseLength === 'FD' || baseLength === 'FE' || baseLength === 'FF') {
+      let length = 0
+      const byteLength = determineByteLength(baseLength)
+      for (let i = 0; i < byteLength; i++) {
+        length += parseInt(setIteratedByte(), 16)
+      }
+      return length
+    }
+
+    return parseInt(baseLength, 16)
+  }
+
+  return stackItemIterator()
+}
+
+const determineByteLength = baseLength => {
+  if (baseLength === 'FD') return 2
+  else if (baseLength === 'FE') return 4
+  else if (baseLength === 'FF') return 8
+}
+
+// StackItemType getter
+const determineType = byte => StackItemType[byte]
+
+// Determine if there's a nested set based on type
+const determineChildren = type => {
+  if (type === 'Array' || type === 'Struct') return true
+  // TODO: map
+  return false
+}
+
+// This sets the type and base value for _result
+const setResultBase = byte => {
+  const type = determineType(byte)
+  let value
+
+  if (type === 'Array' || type === 'Struct') {
+    value = []
+  } else if (type === 'ByteArray') {
+    value = ''
+  } else if (type === 'Integer') {
+    value = 0
+  } else {
+    // Initializing boolean to undefined, because we'd never use a push or += equivalent with booleans
+    value = undefined
+  }
+
+  return { type, value }
+}
+
+export default Deserialize
---
+++ src/sc/StackItemType.js
@@ -0,0 +1,17 @@
+/**
+ * Enum for StackItem types
+ * @readonly
+ * @enum {string}
+ */
+
+const StackItemtype = {
+  '00': 'ByteArray',
+  '01': 'Boolean',
+  '02': 'Integer',
+  '04': 'InteropInterface',
+  '80': 'Array',
+  '81': 'Struct',
+  '82': 'Map'
+}
+
+export default StackItemtype
