b9047b86570fd4ff966be08b21771736e1f78a8d
Jeroen Peeters <jeroen.peeters@evance.be>
2018-06-04 09:47:38 +0200

    chore(app): make typings tests work

--- src/sc/index.d.ts
+++ src/sc/index.d.ts
@@ -1,6 +1,6 @@
 export * from '../utils'
 export * from './typings/ContractParam';
 export * from './typings/core';
-export * from './typings/Deserialize';
+export * from './typings/fDeserialize';
 export * from './typings/OpCode';
 export * from './typings/ScriptBuilder';
