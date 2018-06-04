3c35fafa2f605927bcea23a2c0f87c14dc6fccb4
Jeroen Peeters <jeroen.peeters@evance.be>
2018-06-04 09:34:10 +0200

    chore(app): fix typing naming

--- src/sc/index.d.ts
+++ src/sc/index.d.ts
@@ -1,6 +1,6 @@
 export * from '../utils'
 export * from './typings/ContractParam';
 export * from './typings/core';
-export * from './typings/unserialize';
+export * from './typings/deserialize';
 export * from './typings/OpCode';
 export * from './typings/ScriptBuilder';
