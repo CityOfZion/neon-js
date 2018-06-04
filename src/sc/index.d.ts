90def7c48a0ea8a88ba3c6e35442d48088c611e8
Jeroen Peeters <jeroen.peeters@evance.be>
2018-06-04 09:53:06 +0200

    chore(app): rename typings

--- src/sc/index.d.ts
+++ src/sc/index.d.ts
@@ -1,6 +1,6 @@
 export * from '../utils'
 export * from './typings/ContractParam';
 export * from './typings/core';
-export * from './typings/fDeserialize';
+export * from './typings/deserialize';
 export * from './typings/OpCode';
 export * from './typings/ScriptBuilder';
