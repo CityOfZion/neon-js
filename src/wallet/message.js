7230f5ad1c7811f2800f8aaa5b054c7d02b8171f
Vitor Nazário Coelho <vncoelho@gmail.com>
2018-08-12 11:29:15 -0300

    Fixing const

--- src/wallet/message.js
+++ src/wallet/message.js
@@ -28,8 +28,8 @@ export const signMessage = (message, privateKey) => {
   if (!isPrivateKey(privateKey) && !isWIF(privateKey)) throw new Error('Invalid private key or WIF')
   if (isWIF(privateKey)) privateKey = getPrivateKeyFromWIF(privateKey)
 
-  const messageHex = message
-  if (!isHex(message)) messageHex = str2hexstring(message)
+  messageHex = message
+  if (!isHex(messageHex)) messageHex = str2hexstring(message)
   return generateSignature(messageHex, privateKey)
 }
 
@@ -47,8 +47,8 @@ export const verifyMessage = (message, signature, publicKey) => {
 
   const ecdsa = new EC('p256')
   const sig = getSignatureFromHex(signature)
-  const messageHex = message
-  if (!isHex(message)) messageHex = str2hexstring(message)
+  messageHex = message
+  if (!isHex(messageHex)) messageHex = str2hexstring(message)
   const messageHash = sha256(messageHex)
   return ecdsa.verify(messageHash, sig, publicKey, 'hex')
 }
