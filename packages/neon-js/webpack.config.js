c5cd40820ddefb47122dd41bef5dfe04dbc66a52
Yak Jun Xiang <snowypowers@gmail.com>
2018-08-19 14:45:09 -0700

    refactor(all): Update webpack configuration
    - Use common config located at root
    - Do not minify prod nodejs output

--- packages/neon-js/webpack.config.js
+++ packages/neon-js/webpack.config.js
@@ -1,49 +1,28 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const common = require("../../webpack.common");
 
-let env = process.env.NODE_ENV || 'development'
-
-let common = {
-  mode: env,
-  devtool: env === 'development' ? 'source-map' : false,
-  entry: './src/index.ts',
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          compilerOptions: {
-            declarationDir: 'dist',
-            sourceMap: env === 'development'
-          }
-        }
-      }
-    ]
-  },
-  plugins: [new CleanWebpackPlugin(['dist'])]
-}
-
-module.exports = [
-  Object.assign({}, common, {
-    target: 'node',
+const base = common(__dirname);
+module.exports = function() {
+  const nodeOutput = Object.assign({}, base, {
+    target: "node",
     output: {
       path: __dirname,
-      filename: './dist/index.js',
-      libraryTarget: 'umd'
+      filename: "./dist/index.js",
+      libraryTarget: "umd"
     }
-  }),
-  Object.assign({}, common, {
-    target: 'web',
+  });
+  nodeOutput.optimization = Object.assign({}, nodeOutput.optimization, {
+    minimize: false
+  });
+
+  const webOutput = Object.assign({}, base, {
+    target: "web",
     output: {
       path: __dirname,
-      filename: './dist/browser.js',
-      libraryTarget: 'umd',
-      library: 'Neon' // This is the var name in browser
+      filename: "./dist/browser.js",
+      libraryTarget: "umd",
+      library: "Neon" // This is the var name in browser
     }
-  })
-]
+  });
+
+  return [nodeOutput, webOutput];
+};
