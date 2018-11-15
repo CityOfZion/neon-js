8478b643d7c3f9d6486328b0a46ec88449f7e495
Merl <merl111@protonmail.com>
2018-11-15 13:14:51 +0100

    fix, added urls to test and removed urls.ts

--- packages/neon-domain/__integration__/NeoNS.ts
+++ packages/neon-domain/__integration__/NeoNS.ts
@@ -1,9 +1,33 @@
 import * as neonCore from "@cityofzion/neon-core";
 import domainPlugin from "../src/index";
-import { getMainnetUrls } from "./urls";
 
 const neonJs = domainPlugin(neonCore);
 const { CONST, rpc, u, api, wallet } = neonJs;
+
+export function getMainnetUrls(): string[] {
+    return [
+        "http://seed1.ngd.network:10332",
+        "http://seed2.ngd.network:10332",
+        "http://seed3.ngd.network:10332",
+        "http://seed4.ngd.network:10332",
+        "http://seed5.ngd.network:10332",
+        "http://seed6.ngd.network:10332",
+        "http://seed7.ngd.network:10332",
+        "http://seed8.ngd.network:10332",
+        "http://seed9.ngd.network:10332",
+        "http://seed10.ngd.network:10332",
+        "https://seed0.cityofzion.io:443",
+        "https://seed1.cityofzion.io:443",
+        "https://seed2.cityofzion.io:443",
+        "https://seed3.cityofzion.io:443",
+        "https://seed4.cityofzion.io:443",
+        "https://seed5.cityofzion.io:443",
+        "https://seed6.cityofzion.io:443",
+        "https://seed7.cityofzion.io:443",
+        "https://seed8.cityofzion.io:443",
+        "https://seed9.cityofzion.io:443"
+    ]
+};
 
 let MAINNET_URL = "";
 let provider;
