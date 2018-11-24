259b724ae29e522eff20099d784aa722abd37190
Yak Jun Xiang <snowypowers+github@gmail.com>
2018-11-23 19:46:33 -0800

    docs(guides): Rework guides (#360)
    - Add examples
    - Generate guides from examples
    - Upgrade docusaurus to 1.5.1

--- examples/basic/claimgas.js
+++ examples/basic/claimgas.js
@@ -1,3 +1,4 @@
+/**
 ---
 id: claimgas
 title: Basic - Claiming Gas
@@ -5,39 +6,41 @@ title: Basic - Claiming Gas
 
 This is a basic tutorial to claim gas using the `claimGas` method. If you have read `Basic - Sending Assets`, then this should feel familiar. The process is similar and is even easier.
 
-> NOTE: This does not automatically send your NEO to yourself. It is merely performing the claim action for any claimable GAS.
+Claiming of GAS is built into the NEO blockchain and done through the Claim Transaction. In order to claim GAS, you must have some spent NEO UTXO. This means that you should have sent some NEO from the claiming address to any address (this includes yourself) after your last claim. You are unable to claim GAS for any NEO that has not been moved.
 
-```js
+> NOTE: This does not automatically send your NEO to yourself. It is merely performing the claim action for any claimable GAS.
+*/
+
 // import Neon, { api, wallet } from "@cityofzion/neon-js";
 const { default: Neon, api, wallet } = require("@cityofzion/neon-js");
 
 const claimingPrivateKey =
   "9ab7e154840daca3a2efadaf0df93cd3a5b51768c632f5433f86909d9b994a69";
 const network = "TestNet";
-```
 
+/**
 ## Selecting the API provider and network
 Like in `sendAsset`, we establish the network we are interacting with through the selection of a API provider.
+*/
 
-```js
 const apiProvider = new api.neoscan.instance("TestNet");
 
 console.log("\n\n--- API Provider ---");
 console.log(apiProvider);
-```
 
+/**
 ## Creating the Account
 Similar to `sendAsset`, we are using an Account object to encapsulate our private key.
 
 This account will be the account to claim gas from.
+*/
 
-```js
 const account = new wallet.Account(claimingPrivateKey);
 
 console.log("\n\n--- Claiming Address ---");
 console.log(account);
-```
 
+/**
 ## Execute
 We assemble the config object and claimGas will do the following:
 
@@ -46,11 +49,11 @@ We assemble the config object and claimGas will do the following:
 3. Assemble the transaction using the claims.
 4. Sign the transaction using the claimingPrivateKey
 5. Submit the transaction to the network using the `sendrawtransaction` RPC call to the rpc url.
+*/
 
-```js
 const config = {
-  api: apiProvider,
-  account: account
+  api: apiProvider, // The API Provider that we rely on for balance and rpc information
+  account: account // The claiming Account
 };
 
 Neon.claimGas(config)
@@ -61,7 +64,8 @@ Neon.claimGas(config)
   .catch(config => {
     console.log(config);
   });
-```
 
+/**
 ## Notes
 While it is possible to claim gas and send it to another address, this method assumes that you are claiming it and sending it to the same address.
+*/
--- examples/basic/sendasset.js
+++ examples/basic/sendasset.js
@@ -1,3 +1,4 @@
+/**
 ---
 id: sendasset
 title: Basic - Sending assets
@@ -10,11 +11,18 @@ title: Basic - Sending assets
 3. Invoking smart contracts
 4. Setting up your votes
 
-In this guide, we will be running through how to use `sendAsset` API method.
+These packaged functionalities come from the `neon-api` package. The methods are:
+
+1. sendAsset
+2. claimGas
+3. doInvoke
+4. setupVote
+
+For this guide, we will be going through sending assets using `sendAsset`.
 
 In NEO, the normal transaction for transferring UTXO assets is the Contract Transaction. The `sendAsset` method will help create the Contract Transaction by pulling the necesssary data from a 3rd party data source (like neoscan), assembling the transaction and sending it to the network.
+*/
 
-```js
 // import Neon, { api, wallet } from "@cityofzion/neon-js";
 const { default: Neon, api, wallet } = require("@cityofzion/neon-js");
 
@@ -22,45 +30,45 @@ const sendingKey =
   "9ab7e154840daca3a2efadaf0df93cd3a5b51768c632f5433f86909d9b994a69";
 const receivingAddress = "ALq7AWrhAueN6mJNqk6FHJjnsEoPRytLdW";
 const network = "TestNet";
-```
 
+/**
 ## Creating intents
 To send an asset, we first have to create an `Intent`. Intents represent the instructions to send assets to a specific address.
 
 > Do note that intents are only used for UTXO assets. If you are looking to send NEP5 tokens, this is not the correct way.
+*/
 
-```js
 // We want to send 1 NEO and 1 GAS to ALq7AWrhAueN6mJNqk6FHJjnsEoPRytLdW
 const intent = api.makeIntent({ NEO: 1, GAS: 0.00000001 }, receivingAddress);
 
 console.log("\n\n--- Intents ---");
 intent.forEach(i => console.log(i));
-```
 
+/**
 To add more intents, simple use`api.makeIntent` to create your intents. The method returns an array of intent objects so make sure to concatenate them together.
 
 ## Selecting the API provider and network
 We need to decide which provider shall we use. In this example, I shall be using `TestNet` with neoscan. As `TestNet` is built into the `neon-js` networks, we can retrieve it using its name `TestNet` instead of stating a url. For other networks such as your own private network, you will input the url of your private neoscan (for example, https://localhost:4000/api/main_net)
+*/
 
-```js
 const apiProvider = new api.neoscan.instance(network);
 
 console.log("\n\n--- API Provider ---");
 console.log(apiProvider);
-```
 
+/**
 ## Creating the Account
 We create an `Account` object to store our credentials. In this example, we are using a private key to create the account because we are also signing with the same private key. However, if you wish to use an external signing function (such as a ledger), you can create the account with an address and provide the signing function instead.
 
 This account will be the account where the assets will come from.
+*/
 
-```js
 const account = new wallet.Account(sendingKey);
 
 console.log("\n\n--- Sending Address ---");
 console.log(account);
-```
 
+/**
 ## Execute
 We assemble the config object and sendAsset will do the following:
 
@@ -69,12 +77,12 @@ We assemble the config object and sendAsset will do the following:
 3. Assemble the transaction using the balance and intents.
 4. Sign the transaction using the sendingKey
 5. Submit the transaction to the network using the `sendrawtransaction` RPC call to the rpc url.
+*/
 
-```js
 const config = {
-  api: apiProvider, // The network to perform the action, MainNet or TestNet.
-  account: account, // This is the address which the assets come from.
-  intents: intent // This is where you want to send assets to.
+  api: apiProvider, // The API Provider that we rely on for balance and rpc information
+  account: account, // The sending Account
+  intents: intent // Our sending intents
 };
 
 Neon.sendAsset(config)
@@ -85,8 +93,9 @@ Neon.sendAsset(config)
   .catch(config => {
     console.log(config);
   });
-```
 
+/**
 `sendAsset` automatically does all the work in retrieving the balance, constructing the transaction, signing it and sending it off to the optimal node. When this is done, the promise returns the `config` object. The object is the same object passed in at the beginning with all the additional information used in the process appended to it. For example, the `url` field is the NEO node that sent the transaction to.
 
 Here, we are interested in the `response` property, which contains the response from the RPC endpoint upon sending the transaction. If the transaction is successful, the `txid` is attached in the response. We can take this string and use it in a block explorer to check out our transaction.
+*/
