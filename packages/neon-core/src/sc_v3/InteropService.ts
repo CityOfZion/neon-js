5b1a1a83e32bb1263fdcaaf927ec4657e8a187a1
Wafei <34429036+WafeiBaBa@users.noreply.github.com>
2019-07-16 14:37:51 +0800

    update: wallet multisig

--- packages/neon-core/src/sc_v3/InteropService.ts
+++ packages/neon-core/src/sc_v3/InteropService.ts
@@ -1,6 +1,7 @@
 export enum InteropService {
   SYSTEM_CONTRACT_CALL = "System.Contract.Call",
-  NEO_CONTRACT_CREATE = "Neo.Contract.Create"
+  NEO_CONTRACT_CREATE = "Neo.Contract.Create",
+  NEO_CRYPTO_CHECKMULTISIG = "Neo.Crypto.CheckMultiSig"
 }
 
 export default InteropService;
