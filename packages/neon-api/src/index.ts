68680bb6feca88f45053c51a5545aedd7285a2f5
MorganDream <dreameng023@gmail.com>
2019-10-15 17:17:27 +0800

    remove providers temporarily

--- packages/neon-api/src/index.ts
+++ packages/neon-api/src/index.ts
@@ -1,7 +1,6 @@
 import * as _Neon from "@cityofzion/neon-core";
 import { default as apiSettings } from "./settings";
 import { TransactionBuilder } from "./transationBuilder";
-import { NetProvider, EntityProvider } from "./providers";
 
 function assignSettings(
   baseSettings: typeof _Neon.settings,
@@ -24,15 +23,11 @@ function bundle<T extends typeof _Neon>(
   neonCore: T
 ): T & {
   TransactionBuilder: TransactionBuilder;
-  NetProvider: NetProvider;
-  EntityProvider: EntityProvider;
 } {
   assignSettings(neonCore.settings, apiSettings);
   return {
     ...(neonCore as any),
-    TransactionBuilder,
-    NetProvider,
-    EntityProvider
+    TransactionBuilder
   };
 }
 
--- packages/neon-api/src/providers/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./NetProvider";
-export * from "./EntityProvider";
