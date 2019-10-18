306a70c0aa6004daeaade30719db5a712f0f1fce
MorganDream <dreameng023@gmail.com>
2019-10-18 16:35:27 +0800

    remvoe reverse; isEqualTo => equals; add doc

--- packages/neon-core/src/u/HexString.ts
+++ packages/neon-core/src/u/HexString.ts
@@ -17,6 +17,11 @@ export class HexString {
     ensureHex(value);
   }
 
+  /**
+   * Initiate a HexString
+   * @param value
+   * @param littleEndian tell whether value is little endian or not. default to be false.
+   */
   protected constructor(value: string, littleEndian = false) {
     if (value.startsWith("0x")) {
       value = value.slice(2);
@@ -25,52 +30,89 @@ export class HexString {
     this._value = littleEndian ? reverseHex(value) : value;
   }
 
+  /**
+   * Export as big endian string
+   */
   public toBigEndian(): string {
     return this._value;
   }
 
+  /**
+   * Export as little endian string
+   */
   public toLittleEndian(): string {
     return reverseHex(this._value);
   }
 
-  public reverse(): this {
-    this._value = reverseHex(this._value);
-    return this;
-  }
-
-  public isEqualTo(other: HexString): boolean {
+  /**
+   * Judge if 2 HexString are equal
+   * @param other
+   */
+  public equals(other: HexString): boolean {
     return this.toBigEndian() === other.toBigEndian();
   }
 
+  /**
+   * XOR with another HexString to get a new one.
+   * @param other
+   */
   public xor(other: HexString): HexString {
     return HexString.fromHex(hexXor(this.toBigEndian(), other.toBigEndian()));
   }
 
+  /**
+   * Export as ASCII string
+   */
   public toAscii(): string {
     return hexstring2str(this.toBigEndian());
   }
 
+  /**
+   * Export as number
+   */
   public toNumber(): number {
     return parseInt(this.toBigEndian(), 16);
   }
 
+  /**
+   * Export to ArrayBuffer in Uint8Array
+   */
   public toArrayBuffer(): Uint8Array {
     return hexstring2ab(this.toBigEndian());
   }
 
+  /**
+   * Get HexString instance from a hex string
+   * @param str hex string
+   * @param littleEndian whether `str` is little endian
+   */
   public static fromHex(str: string, littleEndian = false) {
     return new HexString(str, littleEndian);
   }
 
+  /**
+   * Get HexString instance from a ASCII string
+   * @param str
+   */
   public static fromAscii(str: string) {
     const hex = str2hexstring(str);
     return new HexString(hex);
   }
 
+  /**
+   * Get HexString instance from a number
+   * @param num
+   * @param littleEndian whether `num` is little endian
+   */
   public static fromNumber(num: number, littleEndian = false) {
     return new HexString(num2hexstring(num), littleEndian);
   }
 
+  /**
+   * Get HexString instance from array buffer
+   * @param arr
+   * @param littleEndian whether `arr` is little endian
+   */
   public static fromArrayBuffer(
     arr: ArrayBuffer | ArrayLike<number>,
     littleEndian = false
