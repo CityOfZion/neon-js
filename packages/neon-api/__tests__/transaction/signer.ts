7bd81447826a96d087c1daaab5667289766337a1
MorganDream <dreameng023@gmail.com>
2019-11-13 15:43:17 +0800

    refator codes; add check before signing

--- packages/neon-api/__tests__/transaction/signer.ts
+++ packages/neon-api/__tests__/transaction/signer.ts
@@ -1,10 +1,6 @@
 import { TransactionSigner, TransactionBuilder } from "../../src/transaction";
 import { wallet, tx, u } from "@cityofzion/neon-core";
 
-const createTransaction = () => {
-  return new TransactionBuilder({ nonce: 1 }).build();
-};
-
 const PRIVATE_KEYS = [
   "9ab7e154840daca3a2efadaf0df93cd3a5b51768c632f5433f86909d9b994a69",
   "ea348e0198f0979ffa5bca238c0a03dc0f72d2681e3378fee4ab27effaf220d6",
@@ -16,12 +12,12 @@ const ACCOUNTS = PRIVATE_KEYS.map(key => new wallet.Account(key));
 const ACCOUNTS_WITNESSES: Array<tx.WitnessLike> = [
   {
     invocationScript:
-      "4049f90f231be07b102041f85e93c5738921e884657b665f98210284fb0e5d017697e140ba6539abec41f9683605d3fff76d7063144c84026c7823c6e99624a4f9",
+      "40f52d1206315dfac64c14ec2dfef1edd62f4460487c23be6bbbbf9080973784ca7dbfe4dfcf4b6b82f2921b968e0d693020b76be0b20171ac56e7da50ab1c4b06",
     verificationScript: ACCOUNTS[0].contract.script
   },
   {
     invocationScript:
-      "4003b00c3d45fc65d06bd05ec9cedfc4a9b1fbb567c30949bffc9c4c7472a76c0b86d678f46c17a46e26de2b92af7728949036bd6215a3a42428c44e1d1098e60a",
+      "40efe3ccf3a49dd670d8785a12218324f60a6b56ed5a628f15522b883a81b51ea9256c0be62008377b156eb1a76e6dc25aad776524c18eb01b0810ed833b15a1ca",
     verificationScript: ACCOUNTS[1].contract.script
   }
 ];
@@ -31,67 +27,115 @@ const MULTISIG_ACCOUNT = wallet.Account.createMultiSig(
   ACCOUNTS.map(account => account.publicKey)
 );
 
+const createTransactionSigner = () => {
+  return new TransactionSigner(
+    new TransactionBuilder({
+      nonce: 1,
+      sender: u.reverseHex(ACCOUNTS[0].scriptHash),
+      cosigners: [
+        {
+          account: u.reverseHex(ACCOUNTS[1].scriptHash),
+          scopes: tx.WitnessScope.Global
+        },
+        {
+          account: u.reverseHex(ACCOUNTS[2].scriptHash),
+          scopes: tx.WitnessScope.Global
+        }
+      ]
+    }).build()
+  );
+};
+
 describe("signWithAccount", () => {
   test("single private key", () => {
-    const transaction = createTransaction();
-    TransactionSigner.signWithAccount(transaction, PRIVATE_KEYS[0]);
-    expect(transaction.scripts[0].export()).toEqual(ACCOUNTS_WITNESSES[0]);
+    const signer = createTransactionSigner();
+    signer.signWithAccount(PRIVATE_KEYS[0]);
+    expect(signer.transaction.scripts[0].export()).toEqual(
+      ACCOUNTS_WITNESSES[0]
+    );
   });
 
   test("single account", () => {
-    const transaction = createTransaction();
-    TransactionSigner.signWithAccount(transaction, ACCOUNTS[1]);
-    expect(transaction.scripts[0].export()).toEqual(ACCOUNTS_WITNESSES[1]);
+    const signer = createTransactionSigner();
+    signer.signWithAccount(ACCOUNTS[1]);
+    expect(signer.transaction.scripts[0].export()).toEqual(
+      ACCOUNTS_WITNESSES[1]
+    );
   });
 
   test("multi private keys or accounts", () => {
-    const transaction = createTransaction();
-    TransactionSigner.signWithAccount(
-      transaction,
-      PRIVATE_KEYS[0],
-      ACCOUNTS[1]
+    const signer = createTransactionSigner();
+    signer.signWithAccount(PRIVATE_KEYS[0], ACCOUNTS[1]);
+    expect(signer.transaction.scripts[0].export()).toEqual(
+      ACCOUNTS_WITNESSES[0]
     );
-    expect(transaction.scripts[0].export()).toEqual(ACCOUNTS_WITNESSES[0]);
-    expect(transaction.scripts[1].export()).toEqual(ACCOUNTS_WITNESSES[1]);
+    expect(signer.transaction.scripts[1].export()).toEqual(
+      ACCOUNTS_WITNESSES[1]
+    );
+  });
+
+  test("invalid signing account", () => {
+    const signer = createTransactionSigner();
+    const signWithInvlidAccount = () => {
+      signer.signWithAccount(new wallet.Account());
+    };
+    expect(signWithInvlidAccount).toThrowError();
   });
 });
 
 describe("signWithWitness", () => {
   test("single witness", () => {
-    const transaction = createTransaction();
-    TransactionSigner.signWithWitness(
-      transaction,
-      new tx.Witness(ACCOUNTS_WITNESSES[0])
+    const signer = createTransactionSigner();
+    signer.signWithWitness(new tx.Witness(ACCOUNTS_WITNESSES[0]));
+    expect(signer.transaction.scripts[0].export()).toEqual(
+      ACCOUNTS_WITNESSES[0]
     );
-    expect(transaction.scripts[0].export()).toEqual(ACCOUNTS_WITNESSES[0]);
   });
 
   test("multiple witnesses", () => {
-    const transaction = createTransaction();
-    TransactionSigner.signWithWitness(
-      transaction,
+    const signer = createTransactionSigner();
+    signer.signWithWitness(
       new tx.Witness(ACCOUNTS_WITNESSES[0]),
       new tx.Witness(ACCOUNTS_WITNESSES[1])
     );
-    expect(transaction.scripts[0].export()).toEqual(ACCOUNTS_WITNESSES[0]);
-    expect(transaction.scripts[1].export()).toEqual(ACCOUNTS_WITNESSES[1]);
+    expect(signer.transaction.scripts[0].export()).toEqual(
+      ACCOUNTS_WITNESSES[0]
+    );
+    expect(signer.transaction.scripts[1].export()).toEqual(
+      ACCOUNTS_WITNESSES[1]
+    );
   });
 });
 
 describe("addMultiSig", () => {
   test("addMultiSig", () => {
-    const transaction = createTransaction();
-    transaction.sender = u.reverseHex(MULTISIG_ACCOUNT.scriptHash);
-    TransactionSigner.addMultiSig(
-      transaction,
+    const signer = new TransactionSigner(
+      new TransactionBuilder({
+        nonce: 1,
+        sender: u.reverseHex(MULTISIG_ACCOUNT.scriptHash)
+      }).build()
+    );
+    signer.addMultiSig(
       MULTISIG_ACCOUNT,
       ...ACCOUNTS_WITNESSES.map(obj => new tx.Witness(obj))
     );
-    expect(transaction.scripts[0].export()).toEqual({
+    expect(signer.transaction.scripts[0].export()).toEqual({
       invocationScript:
-        "4049f90f231be07b102041f85e93c5738921e884657b665f98210284fb0e5d017697e140ba6539abec41f9683605d3fff76d7063144c84026c7823c6e99624a4f94003b00c3d45fc65d06bd05ec9cedfc4a9b1fbb567c30949bffc9c4c7472a76c0b86d678f46c17a46e26de2b92af7728949036bd6215a3a42428c44e1d1098e60a",
+        "40f52d1206315dfac64c14ec2dfef1edd62f4460487c23be6bbbbf9080973784ca7dbfe4dfcf4b6b82f2921b968e0d693020b76be0b20171ac56e7da50ab1c4b0640efe3ccf3a49dd670d8785a12218324f60a6b56ed5a628f15522b883a81b51ea9256c0be62008377b156eb1a76e6dc25aad776524c18eb01b0810ed833b15a1ca",
       verificationScript:
         "5221031d8e1630ce640966967bc6d95223d21f44304133003140c3b52004dc981349c92103767002bb9f74317035ce8d557a3aed30ce831eb16b5f636a139dad0b07916bed210329898e6e5e0a2f175e205b4019c500d6bb69203b56470ec2fc8ab0a4c065e16d5368c7c34cba"
     });
+  });
+
+  test("invalid addMultiSig", () => {
+    const signer = createTransactionSigner();
+    const signWithMultiAcc = () => {
+      signer.addMultiSig(
+        MULTISIG_ACCOUNT,
+        ...ACCOUNTS_WITNESSES.map(obj => new tx.Witness(obj))
+      );
+    };
+
+    expect(signWithMultiAcc).toThrowError();
   });
 });
--- packages/neon-api/src/transaction/signer.ts
+++ packages/neon-api/src/transaction/signer.ts
@@ -1,53 +1,80 @@
-import { tx, wallet } from "@cityofzion/neon-core";
-import { checkMultisigAcc } from "./util";
+import { tx, wallet, u } from "@cityofzion/neon-core";
 
 /**
- * A class with static functions to sign transaction
+ * A class with functions to sign transaction
  */
 export class TransactionSigner {
   /**
+   * transaction to be signed
+   */
+  public transaction: tx.Transaction;
+
+  constructor(transaction: tx.Transaction) {
+    this.transaction = transaction;
+  }
+
+  /**
    * Sign a transaction with Accounts or Private Keys
    * @description This is used when you have full access to signer accounts
-   * @param transaction transaction to be signed
    * @param accounts accounts that will sign this transaction
    */
-  public static signWithAccount(
-    transaction: tx.Transaction,
-    ...accounts: (wallet.Account | string)[]
-  ) {
-    accounts.forEach(account => transaction.sign(account));
+  public signWithAccount(...accounts: (wallet.Account | string)[]) {
+    accounts.forEach(account => {
+      this._checkAcc(account);
+      this.transaction.sign(account);
+    });
   }
 
   /**
    * Sign a transaction with Witnesses
    * @description This can be used when you accept a signature from someone else
-   * @param transaction transaction to be signed
    * @param witnesses witnesses that will be added to the transaction
    */
-  public static signWithWitness(
-    transaction: tx.Transaction,
-    ...witnesses: tx.Witness[]
-  ) {
-    witnesses.forEach(witness => transaction.addWitness(witness));
+  public signWithWitness(...witnesses: tx.Witness[]) {
+    witnesses.forEach(witness => this.transaction.addWitness(witness));
   }
 
   /**
    * Sign a transaction with multi-signatures for multi-sig account
-   * @param transaction transaction to be signed
    * @param multisigAccount multisig account
    * @param witnesses signatures from accounts within the multisig-account
    */
-  public static addMultiSig(
-    transaction: tx.Transaction,
+  public addMultiSig(
     multisigAccount: wallet.Account,
     ...witnesses: tx.Witness[]
   ) {
-    checkMultisigAcc(transaction, multisigAccount);
+    this._checkMultisigAcc(multisigAccount);
     const multisigWitness = tx.Witness.buildMultiSig(
-      transaction.serialize(),
+      this.transaction.serialize(),
       witnesses,
       multisigAccount
     );
-    transaction.addWitness(multisigWitness);
+    this.transaction.addWitness(multisigWitness);
+  }
+
+  private _checkAcc(account: wallet.Account | string) {
+    const acc = new wallet.Account(account);
+    if (!this._getSignerHashes().some(hash => hash === acc.scriptHash)) {
+      throw new Error(`${account} is neither sender nor cosigner`);
+    }
+  }
+
+  private _checkMultisigAcc(multisigAcc: wallet.Account) {
+    if (!multisigAcc.isMultiSig) {
+      throw new Error(`${multisigAcc} is not a multi-sig account`);
+    }
+
+    if (
+      !this._getSignerHashes().some(hash => hash === multisigAcc.scriptHash)
+    ) {
+      throw new Error(`${multisigAcc} is neither sender nor cosigner`);
+    }
+  }
+
+  private _getSignerHashes() {
+    return [
+      this.transaction.sender,
+      ...this.transaction.cosigners.map(cosigner => cosigner.account)
+    ].map(hash => u.reverseHex(hash));
   }
 }
