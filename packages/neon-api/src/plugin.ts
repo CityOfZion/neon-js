27a604d1a70108c6027c5c95ea8d52a100990306
Albert <me@albert.sh>
2020-05-12 10:05:53 +0200

    feat(api): Add notifications API (#514)
    notifications are a way to subscribe to events emitted from an notifications endpoint using websockets.

--- packages/neon-api/src/plugin.ts
+++ packages/neon-api/src/plugin.ts
@@ -1,6 +1,7 @@
 import * as neoCli from "./provider/neoCli";
 import * as neonDB from "./provider/neonDB";
 import * as neoscan from "./provider/neoscan";
+import * as notifications from "./notifications";
 
-export { neoCli, neoscan, neonDB };
+export { neoCli, neoscan, neonDB, notifications };
 export * from "./funcs";
