d98ff7ad5fda9fff7fe48bf69600522364722700
Yak Jun Xiang <snowypowers@gmail.com>
2020-07-21 00:16:49 -0700

    feat(u): Add support for secp256k1 curve (#549)
    Refactored the methods that touches the elliptic curves so that we do not expose too much of the internals. Moved the elliptic stuff into `u` instead. Does not add any support to the creating a secp256k1 account at the moment.
    
    - Move the elliptic and BNjs imports down to u
    - Create the EllipticCurve class and EllipticCurvePreset
    - Add secp256k1 as another usable curve.

--- packages/neon-core/__tests__/wallet/signing.ts
+++ packages/neon-core/__tests__/wallet/signing.ts
@@ -1,26 +1,31 @@
 import { str2hexstring } from "../../src/u";
 import { sign, verify } from "../../src/wallet/signing";
 
+const k = "0400";
 const keyCombinations = [
   [
     "privateKey & EncodedPublicKey",
     "a7b9775c6b9136bf89f63def7eab0c5f2d3d0c9e85492717f54386420cce5aa1",
     "02963fc761eb7135c4593bfc6a0af96d8588b70d8f6ef3af8549181e57772181f5",
+    "16949b7287d4f481897299b9eb6fe80ccdc5849ae1d527e280e76bb98e61ca0745af9429ca60c81194f9aaf53167a7f7fbec1ba149456a973332ba04806957b0",
   ],
   [
     "privateKey & UnencodedPublicKey",
     "793466a3dfe3935a475d02290e37000a3e835f6740f9733e72e979d6e1166e13",
     "04c1a9b2d0580902a6c2d09a8febd0a7a13518a9a61d08183f09ff929b66ac7c26a4ddc2ceb4ddc55ae7b2920f79fdbfe5b91e6184d7d487e71030007a56a302f2",
+    "16949b7287d4f481897299b9eb6fe80ccdc5849ae1d527e280e76bb98e61ca0793e840083599cea53e4ed851e744dabfdbee34c83c3e9173f43f512cc1d09da0",
   ],
   [
     "WIF & UnencodedPublicKey",
     "L2qkBc4ogTZERR4Watg4QoQq37w8fxrVZkYrPk7ZZSoRUZsr9yML",
     "04963fc761eb7135c4593bfc6a0af96d8588b70d8f6ef3af8549181e57772181f5ab872395851e9b1b0dbee1f46c11cc7928912ddb452964099931e05f7f9efd5c",
+    "16949b7287d4f481897299b9eb6fe80ccdc5849ae1d527e280e76bb98e61ca0784645b8267a8dce99ee4d394f9b8d892b3e74038454b2a5268077f8ea79b2676",
   ],
   [
     "WIF & EncodedPublicKey",
     "L1HKLWratxFhX94XSn98JEULQYKGhRycf4nREe3Cs8EPQStF5u9E",
     "02c1a9b2d0580902a6c2d09a8febd0a7a13518a9a61d08183f09ff929b66ac7c26",
+    "16949b7287d4f481897299b9eb6fe80ccdc5849ae1d527e280e76bb98e61ca077caa77c360fafd0c1faa8b4a4ee2b9dacded20b27807fe26d682c96b7b02125b",
   ],
 ];
 
@@ -29,9 +34,15 @@ const messages = ["City of Zion", "Morpheus", "我的密码", "MyL33tP@33w0rd"];
 describe("Key combinations", () => {
   test.each(keyCombinations)(
     "%s",
-    (msg: string, priKey: string, pubKey: string) => {
+    (
+      msg: string,
+      priKey: string,
+      pubKey: string,
+      expectedSignature: string
+    ) => {
       const hex = str2hexstring(msg);
-      const sig = sign(hex, priKey);
+      const sig = sign(hex, priKey, k);
+      expect(sig).toBe(expectedSignature);
       const result = verify(hex, sig, pubKey);
       expect(result).toBe(true);
     }
@@ -45,7 +56,7 @@ describe("Different messages", () => {
     "02963fc761eb7135c4593bfc6a0af96d8588b70d8f6ef3af8549181e57772181f5";
   test.each(messages)("%s", (msg: string) => {
     const hex = str2hexstring(msg);
-    const sig = sign(hex, priKey);
+    const sig = sign(hex, priKey, k);
     const result = verify(hex, sig, pubKey);
     expect(result).toBe(true);
   });
--- packages/neon-core/src/wallet/signing.ts
+++ packages/neon-core/src/wallet/signing.ts
@@ -1,42 +1,33 @@
-import BN from "bn.js";
-import { ec as EC } from "elliptic";
-import { sha256 } from "../u";
+import { sha256, getCurve, EllipticCurvePreset } from "../u";
 import { getPrivateKeyFromWIF, getPublicKeyUnencoded } from "./core";
 import { isPublicKey, isWIF } from "./verify";
 
-export const curve = new EC("p256");
-
-/**
- * Converts signatureHex to a signature object with r & s.
- */
-function getSignatureFromHex(signatureHex: string): { r: BN; s: BN } {
-  const signatureBuffer = Buffer.from(signatureHex, "hex");
-  const r = new BN(signatureBuffer.slice(0, 32).toString("hex"), 16, "be");
-  const s = new BN(signatureBuffer.slice(32).toString("hex"), 16, "be");
-  return { r, s };
-}
+const curve = getCurve(EllipticCurvePreset.SECP256R1);
 
 /**
  * Generates a ECDSA signature from a hexstring using the given private key.
  * @param hex - hexstring to hash.
  * @param privateKey - hexstring or WIF format.
+ * @param k - optional nonce for generating a signature. Providing this value will cause the signature generated to be deterministic.
+ * @returns a 64 byte hexstring made from (r+s)
  */
-export function sign(hex: string, privateKey: string): string {
+export function sign(
+  hex: string,
+  privateKey: string,
+  k?: number | string
+): string {
   if (isWIF(privateKey)) {
     privateKey = getPrivateKeyFromWIF(privateKey);
   }
   const msgHash = sha256(hex);
-  const msgHashHex = Buffer.from(msgHash, "hex");
-  const privateKeyBuffer = Buffer.from(privateKey, "hex");
-
-  const sig = curve.sign(msgHashHex, privateKeyBuffer);
-  return sig.r.toString("hex", 32) + sig.s.toString("hex", 32);
+  const sig = curve.sign(msgHash, privateKey, k);
+  return sig.r + sig.s;
 }
 
 /**
  * Verifies that the message, signature and signing key matches.
  * @param hex - message that was signed.
- * @param sig - ECDSA signature.
+ * @param sig - ECDSA signature in the form of a 64 byte hexstring (r+s).
  * @param publicKey - encoded/unencoded public key of the signing key.
  */
 export function verify(hex: string, sig: string, publicKey: string): boolean {
@@ -46,8 +37,10 @@ export function verify(hex: string, sig: string, publicKey: string): boolean {
   if (!isPublicKey(publicKey, true)) {
     publicKey = getPublicKeyUnencoded(publicKey);
   }
-  const sigObj = (getSignatureFromHex(sig) as unknown) as EC.SignatureOptions;
+  const ecdsaSignature = {
+    r: sig.substr(0, 64),
+    s: sig.substr(64, 64),
+  };
   const messageHash = sha256(hex);
-  const publicKeyBuffer = Buffer.from(publicKey, "hex");
-  return curve.verify(messageHash, sigObj, publicKeyBuffer, "hex");
+  return curve.verify(messageHash, ecdsaSignature, publicKey);
 }
