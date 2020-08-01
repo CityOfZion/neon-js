768b3cffdd9ba19aca7fbad2f40d2ee791e5260c
Yak Jun Xiang <snowypowers@gmail.com>
2020-08-01 01:16:33 -0700

    feat(sc): Update ContractParamType from preview3

--- packages/neon-core/src/sc/ContractParam.ts
+++ packages/neon-core/src/sc/ContractParam.ts
@@ -3,18 +3,22 @@ import { getScriptHashFromAddress, isAddress } from "../wallet";
 import { NeonObject } from "../model";
 
 export enum ContractParamType {
-  Signature = 0x00,
-  Boolean = 0x01,
-  Integer = 0x02,
-  Hash160 = 0x03,
-  Hash256 = 0x04,
-  ByteArray = 0x05,
-  PublicKey = 0x06,
-  String = 0x07,
-  Array = 0x10,
-  Map = 0x12,
-  InteropInterface = 0xf0,
-  Any = 0xfe,
+  Any = 0x00,
+
+  Boolean = 0x10,
+  Integer = 0x11,
+  ByteArray = 0x12,
+  String = 0x13,
+  Hash160 = 0x14,
+  Hash256 = 0x15,
+  PublicKey = 0x16,
+  Signature = 0x17,
+
+  Array = 0x20,
+  Map = 0x22,
+
+  InteropInterface = 0x30,
+
   Void = 0xff,
 }
 
