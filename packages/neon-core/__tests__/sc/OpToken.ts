1d9f61d7c1db4f9c202f0120b53ac38bf73fd51b
Yak Jun Xiang <snowypowers@gmail.com>
2020-09-24 22:57:01 -0700

    fix(wallet): Fix multi signature methods in wallet (#578)
    Using OpTokens, I can now break down the script into opcodes and its params. This PR uses the OpTokens to fix the multi sig methods that have been broken so far.

--- packages/neon-core/__tests__/sc/OpToken.ts
+++ packages/neon-core/__tests__/sc/OpToken.ts
@@ -41,9 +41,44 @@ describe("fromScript", () => {
       "4101020304",
       [new OpToken(OpCode.SYSCALL, "01020304")],
     ],
+    [
+      "multisig script",
+      "120c2102028a99826edc0c97d18e22b6932373d908d323aa7f92656a77ec26e8861699ef0c21031d8e1630ce640966967bc6d95223d21f44304133003140c3b52004dc981349c90c2102232ce8d2e2063dce0451131851d47421bfc4fc1da4db116fca5302c0756462fa130b41138defaf",
+      [
+        new OpToken(OpCode.PUSH2),
+        new OpToken(
+          OpCode.PUSHDATA1,
+          "2102028a99826edc0c97d18e22b6932373d908d323aa7f92656a77ec26e8861699ef"
+        ),
+        new OpToken(
+          OpCode.PUSHDATA1,
+          "21031d8e1630ce640966967bc6d95223d21f44304133003140c3b52004dc981349c9"
+        ),
+        new OpToken(
+          OpCode.PUSHDATA1,
+          "2102232ce8d2e2063dce0451131851d47421bfc4fc1da4db116fca5302c0756462fa"
+        ),
+        new OpToken(OpCode.PUSH3),
+        new OpToken(OpCode.PUSHNULL),
+        new OpToken(OpCode.SYSCALL, "138defaf"),
+      ],
+    ],
   ])("%s", (_: string, script: string, tokens: OpToken[]) => {
     const result = OpToken.fromScript(script);
 
     expect(result).toEqual(expect.arrayContaining(tokens));
+  });
+});
+
+describe("parseInt", () => {
+  test.each([
+    [0, new OpToken(OpCode.PUSH0)],
+    [-1, new OpToken(OpCode.PUSHM1)],
+    [16, new OpToken(OpCode.PUSH16)],
+    [127, new OpToken(OpCode.PUSHINT8, "7f")],
+    [32512, new OpToken(OpCode.PUSHINT16, "007f")],
+  ])("%d", (expected: number, input: OpToken) => {
+    const result = OpToken.parseInt(input);
+    expect(result).toBe(expected);
   });
 });
--- packages/neon-core/src/sc/OpToken.ts
+++ packages/neon-core/src/sc/OpToken.ts
@@ -1,8 +1,12 @@
 import OpCode from "./OpCode";
-import { StringStream } from "../u";
+import { StringStream, HexString } from "../u";
 
 /**
  * A token from tokenizing a VM script. Consists of a OpCode and optional params that follow it.
+ *
+ * @remarks
+ * Currently, most of the functionality here deals with breaking down the script correctly to identify the parts which are data (not opCodes).
+ * An extension of this would be adding an apply function which we can provide a VM context and that should give us a semi working VM.
  *
  * @example
  *
@@ -24,6 +28,26 @@ export class OpToken {
       operations.push(new OpToken(opCode, params));
     }
     return operations;
+  }
+
+  /**
+   * Attempts to convert a OpToken that is parsable to an integer.
+   * @param opToken - token with code that is a PUSHINT[0-9]+ or PUSH[0-9]+.
+   *
+   * @throws Error if OpToken contains an invalid code.
+   */
+  public static parseInt(opToken: OpToken): number {
+    if (opToken.code >= 0 && opToken.code <= 5) {
+      //PUSHINT*
+      // We dont verify the length of the params. Its screwed if its wrong
+      return opToken.params
+        ? HexString.fromHex(opToken.params, true).toNumber()
+        : 0;
+    } else if (opToken.code >= 0x0f && opToken.code <= 0x20) {
+      return opToken.code - 16;
+    } else {
+      throw new Error("given OpToken isnt a parsable integer.");
+    }
   }
 
   constructor(public code: OpCode, public params?: string) {}
