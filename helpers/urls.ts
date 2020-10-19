0f9834891fcfb2eb23fbab07ddcbee794d8ebe4b
Kevin Levitz <kevin@klevitz.com>
2020-10-19 09:01:01 -0400

    URLs now reference local JSON files for return URLs

--- helpers/urls.ts
+++ helpers/urls.ts
@@ -1,53 +1,26 @@
 import { rpc } from "@cityofzion/neon-core";
 
-const TESTNET_URLS = [
-  "https://test1.cityofzion.io:443",
-  "https://test2.cityofzion.io:443",
-  "https://test3.cityofzion.io:443",
-  "http://seed1.ngd.network:20332",
-  "http://seed2.ngd.network:20332",
-  "http://seed3.ngd.network:20332",
-  "http://seed4.ngd.network:20332",
-  "http://seed5.ngd.network:20332",
-  "http://seed6.ngd.network:20332",
-  "http://seed7.ngd.network:20332",
-  "http://seed8.ngd.network:20332",
-  "http://seed9.ngd.network:20332",
-  "http://seed10.ngd.network:20332",
-];
-
-const MAINNET_URLS = [
-  "http://seed1.ngd.network:10332",
-  "http://seed2.ngd.network:10332",
-  "http://seed3.ngd.network:10332",
-  "http://seed4.ngd.network:10332",
-  "http://seed5.ngd.network:10332",
-  "http://seed6.ngd.network:10332",
-  "http://seed7.ngd.network:10332",
-  "http://seed8.ngd.network:10332",
-  "http://seed9.ngd.network:10332",
-  "http://seed10.ngd.network:10332",
-  "https://seed0.cityofzion.io:443",
-  "https://seed1.cityofzion.io:443",
-  "https://seed2.cityofzion.io:443",
-  "https://seed3.cityofzion.io:443",
-  "https://seed4.cityofzion.io:443",
-  "https://seed5.cityofzion.io:443",
-  "https://seed6.cityofzion.io:443",
-  "https://seed7.cityofzion.io:443",
-  "https://seed8.cityofzion.io:443",
-  "https://seed9.cityofzion.io:443",
-];
-
 export function getUrls(net: string): string[] {
   if (net === "TestNet") {
-    return TESTNET_URLS;
+    return parseJson(require("./testServers.json"));
   } else if (net === "MainNet") {
-    return MAINNET_URLS;
+    return parseJson(require("./mainServers.json"));
   } else {
     throw new Error("Expected MainNet or TestNet");
   }
 }
+
+function parseJson(jsonFile): string[] {
+  let returnArr = [];
+  let concatURL: string;
+  for (var i in jsonFile.sites) {
+    if (jsonFile.sites[i].protocol) {
+      concatURL = jsonFile.sites[i].protocol + "://" + jsonFile.sites[i].url + ":" +jsonFile.sites[i].port
+      returnArr.push(concatURL);
+    }
+  }
+  return returnArr;
+};
 
 function cutArray<T>(arr: T[]): T[] {
   const randomStartIndex = Math.floor(Math.random() * arr.length);
@@ -55,7 +28,6 @@ function cutArray<T>(arr: T[]): T[] {
 }
 export async function getUrl(net: string): Promise<string> {
   const orderedUrls = getUrls(net);
-
   const slicedUrls = cutArray(orderedUrls);
   let previousBlockCount = 0;
   for (let i = 0; i < slicedUrls.length; i++) {
