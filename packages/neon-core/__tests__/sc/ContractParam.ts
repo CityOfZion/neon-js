f9ff5efb60cba5f9358e48a83deffafd9c5ba48a
Yak Jun Xiang <snowypowers@gmail.com>
2020-10-30 19:13:54 -0700

    feat(sc): Improve ContractParam with fromJson (#614)
    Rework how ContractParam construction works. Introduce fromJson and restructure the methods to have clear separation of logic. Static methods are now the preferred entry points with fromJson being the common entrypoint.
    
    - Introduce ContractParamJson which is the direct json output coming from c#.
    - Reduce constructor to type validation.
    - Move parsing and transformation logic into static methods.
    - Add fromJson method as a main entrypoint from external systems.
    - Rework ContractParamLike to be a union of the class and json format.
    - Add tests for parseEnum.
    
    Co-authored-by: Erik van den Brink <git@erikvandenbrink.com>

--- packages/neon-core/__tests__/sc/ContractParam.ts
+++ packages/neon-core/__tests__/sc/ContractParam.ts
@@ -1,8 +1,8 @@
 import {
   ContractParam,
+  ContractParamLike,
   ContractParamType,
   likeContractParam,
-  ContractParamLike,
 } from "../../src/sc/ContractParam";
 import { HexString } from "../../src/u";
 
@@ -71,7 +71,7 @@ describe("Static constructors", () => {
         "179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137215",
         "179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137215",
       ],
-    ])("%s", (_msg: string, data: string | number, expected: unknown) => {
+    ])("%s", (_msg: string, data: string | number, expected: string) => {
       const result = ContractParam.integer(data);
 
       expect(result instanceof ContractParam).toBeTruthy();
@@ -102,42 +102,62 @@ describe("Static constructors", () => {
       expect(hexStringValue.toBigEndian()).toBe(expected);
     });
 
-    test("Errors on non-address or scripthash", () => {
-      const thrower = (): ContractParam => ContractParam.hash160("1");
-      expect(thrower).toThrow();
+    test("errors when not 20 bytes", () => {
+      expect(() => ContractParam.hash160(HexString.fromHex("12"))).toThrow(
+        "expected 20 bytes"
+      );
+    });
+  });
+
+  describe("hash256", () => {
+    test("hexstring", () => {
+      const expected = "abcd".repeat(16);
+      const result = ContractParam.hash256(expected);
+
+      expect(result instanceof ContractParam).toBeTruthy();
+      expect(result.type).toBe(ContractParamType.Hash256);
+      expect(result.value).toBeInstanceOf(HexString);
+      const hexStringValue = result.value as HexString;
+      expect(hexStringValue.toBigEndian()).toBe(expected);
+    });
+
+    test("HexString class", () => {
+      const expected = HexString.fromHex("1234".repeat(16));
+      const result = ContractParam.hash256(expected);
+
+      expect(result instanceof ContractParam).toBeTruthy();
+      expect(result.type).toBe(ContractParamType.Hash256);
+      expect(result.value).toBeInstanceOf(HexString);
+      const hexStringValue = result.value as HexString;
+      expect(hexStringValue.equals(expected)).toBeTruthy();
+    });
+
+    test("errors when not 32 bytes", () => {
+      expect(() => ContractParam.hash256(HexString.fromHex("12"))).toThrow(
+        "expected 32 bytes"
+      );
     });
   });
 
   describe("byteArray", () => {
-    test.each([
-      ["bytearray", ["010203"], "010203"],
-      [
-        "address",
-        ["ALfnhLg7rUyL6Jr98bzzoxz5J7m64fbR4s", "address"],
-        "35b20010db73bf86371075ddfba4e6596f1ff35d",
-      ],
-      ["fixed8", [100.012345678, "fixed8"], "88ba1e5402000000"],
-      ["fixed8 (0 decimals)", [1, "fixed8", 0], "0100000000000000"],
-      ["fixed8(4 decimals)", [222.1234, "fixed8", 4], "b2e4210000000000"],
-    ] as [string, [string | number, string?, ...(string | number)[]], string][])(
-      "%s",
-      (
-        _msg: string,
-        data: [string | number, string?, ...(string | number)[]],
-        expected: string
-      ) => {
-        const result = ContractParam.byteArray(...data);
+    test("base64 encoded string", () => {
+      const result = ContractParam.byteArray("NBI=");
 
-        expect(result instanceof ContractParam).toBeTruthy();
-        expect(result.type).toBe(ContractParamType.ByteArray);
-        expect(result.value).toBe(expected);
-      }
-    );
+      expect(result instanceof ContractParam).toBeTruthy();
+      expect(result.type).toBe(ContractParamType.ByteArray);
+      expect(result.value).toBeInstanceOf(HexString);
+      const hexStringValue = result.value as HexString;
+      expect(hexStringValue.toBigEndian()).toEqual("1234");
+    });
 
-    test("errors when exceeds allowed precision for fixed8", () => {
-      const thrower = (): ContractParam =>
-        ContractParam.byteArray(222.12345, "fixed8", 4);
-      expect(thrower).toThrow("wrong precision");
+    test("HexString", () => {
+      const result = ContractParam.byteArray(HexString.fromHex("1234"));
+
+      expect(result instanceof ContractParam).toBeTruthy();
+      expect(result.type).toBe(ContractParamType.ByteArray);
+      expect(result.value).toBeInstanceOf(HexString);
+      const hexStringValue = result.value as HexString;
+      expect(hexStringValue.toBigEndian()).toEqual("1234");
     });
   });
 
@@ -154,18 +174,26 @@ describe("Static constructors", () => {
     });
   });
 
-  test("publicKey", () => {
-    const result = ContractParam.publicKey(
-      "026d3ca98c83dd2490a134ba4f874b59292afaac8abc2f9b34b690fcd2b44648ee"
-    );
+  describe("publicKey", () => {
+    test("valid key", () => {
+      const result = ContractParam.publicKey(
+        "026d3ca98c83dd2490a134ba4f874b59292afaac8abc2f9b34b690fcd2b44648ee"
+      );
 
-    expect(result instanceof ContractParam).toBeTruthy();
-    expect(result.type).toBe(ContractParamType.PublicKey);
-    expect(result.value).toBeInstanceOf(HexString);
-    const hexStringValue = result.value as HexString;
-    expect(hexStringValue.toBigEndian()).toBe(
-      "026d3ca98c83dd2490a134ba4f874b59292afaac8abc2f9b34b690fcd2b44648ee"
-    );
+      expect(result instanceof ContractParam).toBeTruthy();
+      expect(result.type).toBe(ContractParamType.PublicKey);
+      expect(result.value).toBeInstanceOf(HexString);
+      const hexStringValue = result.value as HexString;
+      expect(hexStringValue.toBigEndian()).toBe(
+        "026d3ca98c83dd2490a134ba4f874b59292afaac8abc2f9b34b690fcd2b44648ee"
+      );
+    });
+
+    test("invalid key", () => {
+      expect(() => ContractParam.publicKey("")).toThrow(
+        "expected valid public key"
+      );
+    });
   });
 });
 
@@ -200,34 +228,111 @@ describe("likeContractParam", () => {
       },
       true,
     ],
-    ["ContractParam", new ContractParam({ type: "Integer", value: 1 }), true],
+    ["ContractParam", ContractParam.integer(1), true],
     ["empty", {}, false],
     ["wrong type", { type: "", value: 1 }, false],
     ["missing value", { type: "ByteArray" }, false],
   ])(
     "%s",
-    (
-      msg: string,
-      data: Partial<ContractParam | ContractParamLike>,
-      expected: boolean
-    ) => {
+    (msg: string, data: Partial<ContractParamLike>, expected: boolean) => {
       const result = likeContractParam(data);
       expect(result).toBe(expected);
     }
   );
 });
 
-describe("export", () => {
-  test("exports properly", () => {
-    const testObject = new ContractParam({
-      type: ContractParamType.Integer,
-      value: 1,
-    });
-    const result = testObject.export();
+describe("toJson", () => {
+  test("integer", () => {
+    const testObject = ContractParam.integer(1);
+    const result = testObject.toJson();
 
     expect(result).toEqual({
       type: "Integer",
-      value: 1,
+      value: "1",
+    });
+  });
+
+  test("boolean", () => {
+    const testObject = ContractParam.boolean(true);
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "Boolean",
+      value: true,
+    });
+  });
+
+  test("string", () => {
+    const testObject = ContractParam.string("utf8 string");
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "String",
+      value: "utf8 string",
+    });
+  });
+
+  test("hash160", () => {
+    const testObject = ContractParam.hash160("abcd".repeat(10));
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "Hash160",
+      value: "abcd".repeat(10),
+    });
+  });
+
+  test("hash256", () => {
+    const testObject = ContractParam.hash256("abcd".repeat(16));
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "Hash256",
+      value: "abcd".repeat(16),
+    });
+  });
+
+  test("publicKey", () => {
+    const testObject = ContractParam.publicKey(
+      "02028a99826edc0c97d18e22b6932373d908d323aa7f92656a77ec26e8861699ef"
+    );
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "PublicKey",
+      value:
+        "02028a99826edc0c97d18e22b6932373d908d323aa7f92656a77ec26e8861699ef",
+    });
+  });
+
+  test("byteArray", () => {
+    const testObject = ContractParam.byteArray("/KleJSvmqQtUVGcH5328mz7DYVQ=");
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "ByteArray",
+      value: "/KleJSvmqQtUVGcH5328mz7DYVQ=",
+    });
+  });
+
+  test("array", () => {
+    const testObject = ContractParam.array(
+      ContractParam.integer(999),
+      ContractParam.boolean(false),
+      ContractParam.string("hello world")
+    );
+    const result = testObject.toJson();
+
+    expect(result).toEqual({
+      type: "Array",
+      value: [
+        {
+          type: "Integer",
+          value: "999",
+        },
+        { type: "Boolean", value: false },
+        { type: "String", value: "hello world" },
+      ],
     });
   });
 });
@@ -267,9 +372,9 @@ describe("equals", () => {
     type: "Void",
   };
 
-  const param1 = new ContractParam(obj1);
-  const param2 = new ContractParam(obj2);
-  const param3 = new ContractParam(obj3);
+  const param1 = ContractParam.fromJson(obj1);
+  const param2 = ContractParam.fromJson(obj2);
+  const param3 = ContractParam.fromJson(obj3);
 
   test.each([
     ["Param1 === Param1", param1, param1, true],
