e1df4ef987d3539f7203fd2b4e959e8b6cbcb402
Yak Jun Xiang <snowypowers@gmail.com>
2021-04-27 00:25:48 +0800

    fix(typings): Remove skipLibCheck (#710)
    Remove skipLibCheck from base tsconfig. This enforces lib checks for all files. This fixes a bug where importing neon-js would cause an error when building due to bignumber.js typings missing.
    
    - Re-add bignumber.js as dependency (deprecated but not removed at the moment)
    - Add @tapable workaround for webpack typings
    - Remove old @types/nodes workaround for TextEncoder

--- packages/neon-core/typings/node.d.ts
+++
@@ -1,9 +0,0 @@
-import { EncodeIntoResult } from "util";
-
-declare global {
-  class TextEncoder {
-    readonly encoding: string;
-    encode(input?: string): Uint8Array;
-    encodeInto(input: string, output: Uint8Array): EncodeIntoResult;
-  }
-}
