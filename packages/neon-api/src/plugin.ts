f9662ceda80aa076bf223ec4de1ebc1a83caca92
Yak Jun Xiang <snowypowers@gmail.com>
2021-04-27 22:12:28 +0800

    feat(api): Add dora API support

--- packages/neon-api/src/plugin.ts
+++ packages/neon-api/src/plugin.ts
@@ -1,7 +1,8 @@
 import * as neoCli from "./provider/neoCli";
 import * as neonDB from "./provider/neonDB";
 import * as neoscan from "./provider/neoscan";
+import * as dora from "./provider/dora";
 import * as notifications from "./notifications";
 
-export { neoCli, neoscan, neonDB, notifications };
+export { neoCli, neoscan, neonDB, dora, notifications };
 export * from "./funcs";
