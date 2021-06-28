b1da6e0f047689b42a17bd828109a8d704308b1b
Yak Jun Xiang <snowypowers@gmail.com>
2021-06-28 16:01:29 +0800

    fix(u): rely on crypto-js for random bytes

--- packages/neon-core/src/u/basic/random.ts
+++ packages/neon-core/src/u/basic/random.ts
@@ -1,9 +1,16 @@
-import secureRandom from "secure-random";
-
+import { lib } from "crypto-js";
 /**
- * Generates a arrayBuffer filled with random bits.
+ * Generates a arrayBuffer filled with random bytes.
  * @param length - length of buffer.
  */
 export const generateRandomArray = (length: number): number[] => {
-  return secureRandom(length) as number[];
+  // Round up to nearest multiple of 4 so that the words generated is more than what we need.
+  const numberOfWords = length % 4 === 0 ? length : length + (length % 4);
+
+  // This converts the generated words into a hexstring.
+  const wordArray = lib.WordArray.random(numberOfWords).toString();
+
+  // Chunk the hexstring into chunks of 2 which represents 1 byte each
+  const hexStrings = wordArray.substr(0, length * 2).match(/.{1,2}/g) || [];
+  return hexStrings.map((hexstr) => parseInt(hexstr, 16));
 };
