afc67e03614b3bcaa28accc4afa8beb6571437f1
Yak Jun Xiang <snowypowers@gmail.com>
2021-07-03 15:15:02 +0800

    fix(sc): Fix ContractPermissions toJson dropping 0x (#758)
    ContractPermissionDescriptor uses the UIint classes which maintains the 0x prefix when exporting to JSON. It is possible for the C# code to perform a safe parse using TryParse but currently, it uses a string length check to determine the data type. Thus, a non-prefixed string  fails the length check and errors out. This fixes the export to JSON to maintain the prefix for contract hashes.
    
    Fix #756

--- packages/neon-core/src/sc/manifest/ContractPermission.ts
+++ packages/neon-core/src/sc/manifest/ContractPermission.ts
@@ -4,21 +4,56 @@ export interface ContractPermissionLike {
 }
 
 export interface ContractPermissionJson {
+  /**
+   * 0x-prefixed contract hash (42 characters), public key (66 characters) or '*'.
+   */
   contract: string;
   methods: "*" | string[];
 }
 
 export class ContractPermission {
+  /**
+   * Contract hash (40 characters), public key (66 characters) or '*'
+   */
   public contract: string;
   public methods: "*" | string[];
 
+  /**
+   *  Parses a ContractPermissionDescriptor.
+   * @param jsonDescriptor - descriptor found in JSON format.
+   * @returns a sanitized string
+   */
+  public static parseJsonDescriptor(jsonDescriptor: string): string {
+    switch (true) {
+      case jsonDescriptor.length === 66: // public key of contract manifest group
+      case jsonDescriptor.length === 40: // contract scripthash
+      case jsonDescriptor === "*": // wildcard, means it accept any contract
+        return jsonDescriptor;
+      case jsonDescriptor.length === 42 && jsonDescriptor.indexOf("0x") === 0: // contract with "0x" prefix
+        return jsonDescriptor.slice(2);
+      default:
+        throw new Error(
+          `This is not a ContractPermissionDescriptor: ${jsonDescriptor}`
+        );
+    }
+  }
+
+  /**
+   * Converts an internal ContractPermissionDescriptor to JSON format.
+   * @param descriptor - internal ContractPermissionDescriptor string
+   * @returns JSON formatted string
+   */
+  public static toJsonDescriptor(descriptor: string): string {
+    if (descriptor.length === 40) return `0x${descriptor}`;
+    return descriptor;
+  }
   public fromJson(json: ContractPermissionJson): ContractPermission {
     return new ContractPermission(json);
   }
 
   public constructor(obj: Partial<ContractPermissionLike> = {}) {
     const { contract = "*", methods = "*" } = obj;
-    this.contract = this._formatContract(contract);
+    this.contract = ContractPermission.parseJsonDescriptor(contract);
     this.methods = methods;
   }
 
@@ -34,24 +69,9 @@ export class ContractPermission {
     return this.contract === "*";
   }
 
-  private _formatContract(contract: string): string {
-    switch (true) {
-      case contract.length === 66: // public key of contract manifest group
-      case contract.length === 40: // contract scripthash
-      case contract === "*": // wildcard, means it accept any contract
-        return contract;
-      case contract.length === 42 && contract.indexOf("0x") === 0: // contract with "0x" prefix
-        return contract.slice(2);
-      default:
-        throw new Error(
-          `This is not a ContractPermissionDescriptor: ${contract}`
-        );
-    }
-  }
-
   public toJson(): ContractPermissionJson {
     return {
-      contract: this.contract,
+      contract: ContractPermission.toJsonDescriptor(this.contract),
       methods: this.methods,
     };
   }
