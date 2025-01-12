244d8e0aeb84870159967ce17b84f9a189dd8a26
Yak Jun Xiang <snowypowers@gmail.com>
2025-01-11 19:01:32 -0500

    Upgrade eslint config to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import tsdoceslint from "eslint-plugin-tsdoc";
+import globals from 'globals';
+export default tseslint.config(
+  {
+    // Do not lint build outputs
+    ignores: [
+      "node_modules/",
+      "website/",
+      "examples/",
+      "packages/**/lib/",
+      "packages/**/dist/",
+      "packages/**/node_modules/",
+    ],
+  },
+  eslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      tsdoc: tsdoceslint,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.lint.json'],
+      },
+      globals: {
+        ...globals.jest
+      }
+    },
+    rules: {
+      "tsdoc/syntax": "warn",
+      "@typescript-eslint/explicit-function-return-type": [
+        "warn",
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": ["warn"],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+        },
+      ],
+    },
+  },
+  eslintPluginPrettier,
+);
